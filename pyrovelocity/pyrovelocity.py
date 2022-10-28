5078654f6bbbf3ddc9eaace9da7ec50a12424bda
Cameron Smith <cameron.ray.smith@gmail.com>
2022-10-28 15:34:37 -0400

    add click cli stub
    - [click](https://click.palletsprojects.com/)

--- pyrovelocity/pyrovelocity.py
+++ pyrovelocity/pyrovelocity.py
@@ -1,1 +1,11 @@
 """Main module."""
+import click
+
+from . import __version__
+
+
+@click.command()
+@click.version_option(version=__version__)
+def main():
+    """Command line interface for pyrovelocity."""
+    click.echo("pyrovelocity")
