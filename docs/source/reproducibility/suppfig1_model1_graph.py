db89456ec4005bec2826532aa227e933dabce2fa
Cameron Smith <cameron.ray.smith@gmail.com>
2022-10-31 09:41:20 -0400

    update suppfig1 model1 graph
    - pyrovelocity
    - remove unused code

--- docs/source/reproducibility/suppfig1_model1_graph.py
+++ docs/source/reproducibility/suppfig1_model1_graph.py
@@ -1,28 +1,8 @@
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import scvelo as scv
-import seaborn as sns
+import pyro
 import torch
-from dynamical_velocity2.api import train_model
-from dynamical_velocity2.data import load_data
-from dynamical_velocity2.plot import denoised_umap
-from dynamical_velocity2.plot import plot_arrow_examples
-from dynamical_velocity2.plot import plot_gene_ranking
-from dynamical_velocity2.plot import plot_mean_vector_field
-from dynamical_velocity2.plot import plot_posterior_time
-from dynamical_velocity2.plot import plot_vector_field_uncertain
-from dynamical_velocity2.plot import project_grid_points
-from dynamical_velocity2.plot import rainbowplot
-from dynamical_velocity2.plot import us_rainbowplot
-from dynamical_velocity2.plot import vector_field_uncertainty
-from dynamical_velocity2.utils import mae
-from dynamical_velocity2.utils import mae_evaluate
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-from scipy.stats import pearsonr
-from scipy.stats import spearmanr
-from sklearn.model_selection import train_test_split
+
+from pyrovelocity.api import train_model
+from pyrovelocity.data import load_data
 
 
 adata = load_data(top_n=2000, min_shared_counts=30)
@@ -41,10 +21,6 @@ adata_model_pos = train_model(
     guide_type="auto",
     train_size=1.0,
 )
-import pyro
-
-# Model 2
-import torch
 
 
 demo_data = torch.ones(adata.shape[0], adata.shape[1]).to("cuda:0")
@@ -83,11 +59,6 @@ adata_model_pos = train_model(
     guide_type="auto_t0_constraint",
     train_size=1.0,
 )
-
-import pyro
-
-# Model 1
-import torch
 
 
 demo_data = torch.ones(adata.shape[0], adata.shape[1]).to("cuda:0")
