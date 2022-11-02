0816d328c7dd7668faa6ecf352d3632d81f329e3
Cameron Smith <cameron.ray.smith@gmail.com>
2022-11-02 12:06:45 -0400

    add main terraform module

---
+++ reproducibility/environment/main.tf
@@ -0,0 +1,17 @@
+// PROVIDER ===============================================
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.41.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
