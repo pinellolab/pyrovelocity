94dba0ee14e9f81f77b6f94dbebace8bcf119e99
Cameron Smith <cameron.ray.smith@gmail.com>
2022-11-03 09:40:21 -0400

    add optional mypy nox session

--- noxfile.py
+++ noxfile.py
@@ -132,6 +132,23 @@ def precommit(session: Session) -> None:
         activate_virtualenv_in_precommit_hooks(session)
 
 
+@session(python=python_versions)
+def mypy(session: Session) -> None:
+    """Type-check using mypy."""
+    args = session.posargs or [
+        "--install-types",
+        "--non-interactive",
+        "pyrovelocity",
+        "tests",
+        "docs/conf.py",
+    ]
+    session.install(".")
+    session.install("mypy", "pytest", "types-requests", "pytest-mock")
+    session.run("mypy", *args)
+    if not session.posargs:
+        session.run("mypy", f"--python-executable={sys.executable}", "noxfile.py")
+
+
 @session(name="docs-build", python=python_versions[0])
 def docs_build(session: Session) -> None:
     """Build the documentation."""
