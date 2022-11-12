15786e38d26f2eb2e73164becafdd4ad77ced76b
Cameron Smith <cameron.ray.smith@gmail.com>
2022-11-12 14:17:52 -0500

    remove unused setup
    - see pyproject.toml

--- setup.py
+++ setup.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python
 
-"""The setup script."""
 
-from setuptools import find_packages
+# from setuptools import find_packages
 from setuptools import setup
 
 
-with open("README.md") as readme_file:
-    readme = readme_file.read()
+if __name__ == "__main__":
+    setup(name="pyrovelocity")
 
-with open("docs/history.md") as history_file:
-    history = history_file.read()
+# with open("README.md") as readme_file:
+#     readme = readme_file.read()
 
-requirements = []
+# with open("docs/history.md") as history_file:
+#     history = history_file.read()
 
-test_requirements = []
+# requirements = []
 
-setup(
-    author="Qian Qin",
-    author_email="qqin@mgh.harvard.edu",
-    python_requires="==3.8",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-    ],
-    description="a multivariate RNA Velocity model to estimate the uncertainty of cell future state using Pyro",
-    install_requires=requirements,
-    license="GNU AFFERO GENERAL PUBLIC LICENSE v3",
-    long_description=readme + "\n\n" + history,
-    include_package_data=True,
-    keywords="pyrovelocity",
-    name="pyrovelocity",
-    packages=find_packages(include=["pyrovelocity", "pyrovelocity.*"]),
-    test_suite="tests",
-    tests_require=test_requirements,
-    url="https://github.com/qinqian/pyrovelocity",
-    version="0.1.0",
-    zip_safe=False,
-)
+# test_requirements = []
+
+# setup(
+#     author="Qian Qin",
+#     author_email="qqin@mgh.harvard.edu",
+#     python_requires="==3.8",
+#     classifiers=[
+#         "Development Status :: 2 - Pre-Alpha",
+#         "Intended Audience :: Developers",
+#         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+#         "Natural Language :: English",
+#         "Programming Language :: Python :: 3",
+#         "Programming Language :: Python :: 3.8",
+#     ],
+#     description="a multivariate RNA Velocity model to estimate the uncertainty of cell future state using Pyro",
+#     install_requires=requirements,
+#     license="GNU AFFERO GENERAL PUBLIC LICENSE v3",
+#     long_description=readme + "\n\n" + history,
+#     include_package_data=True,
+#     keywords="pyrovelocity",
+#     name="pyrovelocity",
+#     packages=find_packages(include=["pyrovelocity", "pyrovelocity.*"]),
+#     test_suite="tests",
+#     tests_require=test_requirements,
+#     url="https://github.com/qinqian/pyrovelocity",
+#     version="0.1.0",
+#     zip_safe=False,
+# )
