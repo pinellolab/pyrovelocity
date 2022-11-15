555680e94958bc325619c796a6a9ff38ed895d7a
Cameron Smith <cameron.ray.smith@gmail.com>
2022-11-15 17:37:03 -0500

    support data download from multiple sources
    - assume url provides download source for adata object

--- reproducibility/figures/data_download.py
+++ reproducibility/figures/data_download.py
@@ -4,6 +4,7 @@ from logging import Logger
 from pathlib import Path
 from typing import Text
 
+import scanpy as scp
 import scvelo as scv
 from config import config_setup
 from omegaconf import DictConfig
@@ -12,37 +13,63 @@ from pyrovelocity.utils import get_pylogger
 from pyrovelocity.utils import print_attributes
 
 
-def download_scvelo_datasets(conf: DictConfig, logger: Logger) -> None:
-    for data_set in conf.download:
-        logger.info(
-            f"\n\nVerifying {data_set} data:\n\n"
-            f"  from url {conf[data_set].url}\n"
-            f"  temporarily downloaded to {conf[data_set].dl_path}\n"
-            f"  stored in {conf[data_set].rel_path}\n"
-        )
-
-        if os.path.isfile(conf[data_set].rel_path) and os.access(
-            conf[data_set].rel_path, os.R_OK
-        ):
-            logger.info(f"{conf[data_set].rel_path} exists")
-        else:
-            logger.info(f"downloading {conf[data_set].rel_path} ...")
-            dl_method = getattr(scv.datasets, data_set)
-            adata = dl_method()  # e.g. scv.datasets.pancreas()
-            print_attributes(adata)
-            os.replace(
-                conf[data_set].dl_path,
-                conf[data_set].rel_path,
+def download_datasets(conf: DictConfig, logger: Logger) -> None:
+    for source in conf.sources:
+        for data_set in conf[source].download:
+            logger.info(
+                f"\n\nVerifying {data_set} data:\n\n"
+                f"  from url {conf[source][data_set].url}\n"
+                f"  temporarily downloaded to {conf[source][data_set].dl_path}\n"
+                f"  stored in {conf[source][data_set].rel_path}\n"
             )
-            os.rmdir(conf[data_set].dl_root)
-            logger.info(f"downloaded {conf[data_set].rel_path}")
+
+            if os.path.isfile(conf[source][data_set].rel_path) and os.access(
+                conf[source][data_set].rel_path, os.R_OK
+            ):
+                logger.info(f"{conf[source][data_set].rel_path} exists")
+            else:
+                logger.info(f"downloading {conf[source][data_set].rel_path} ...")
+                if source == "scvelo":
+                    dl_method = getattr(scv.datasets, data_set)
+                    adata = dl_method()  # e.g. scv.datasets.pancreas()
+                else:
+                    adata = scp.read(
+                        conf[source][data_set].rel_path,
+                        backup_url=conf[source][data_set].url,
+                    )
+
+                print_attributes(adata)
+                if conf[source][data_set].dl_path != conf[source][data_set].rel_path:
+                    os.replace(
+                        conf[source][data_set].dl_path,
+                        conf[source][data_set].rel_path,
+                    )
+                    try:
+                        os.rmdir(conf[source][data_set].dl_root)
+                    except OSError as e:
+                        logger.warn(
+                            "%s : %s" % (conf[source][data_set].dl_root, e.strerror)
+                        )
+                        pass
+
+                if os.path.isfile(conf[source][data_set].rel_path) and os.access(
+                    conf[source][data_set].rel_path, os.R_OK
+                ):
+                    logger.info(
+                        f"successfully downloaded {conf[source][data_set].rel_path}"
+                    )
+                else:
+                    logger.warn(
+                        f"cannot find and read {conf[source][data_set].rel_path}"
+                    )
 
 
-def data_download(conf: DictConfig) -> None:
+def main(config_path: str) -> None:
     """Load external data.
     Args:
         config_path {Text}: path to config
     """
+    conf = config_setup(config_path)
 
     logger = get_pylogger(name="DATA_LOAD", log_level=conf.base.log_level)
     logger.info(
@@ -53,12 +80,7 @@ def data_download(conf: DictConfig) -> None:
     Path(conf.data_external.root_path).mkdir(parents=True, exist_ok=True)
     Path(conf.data_processed.root_path).mkdir(parents=True, exist_ok=True)
 
-    download_scvelo_datasets(conf.data_external.scvelo, logger)
-
-
-def main(config_path: str) -> None:
-    conf = config_setup(config_path)
-    data_download(conf)
+    download_datasets(conf.data_external, logger)
 
 
 if __name__ == "__main__":
