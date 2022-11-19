b62a7fb97735882e9624a591ee9cd6b1b7ba59ca
Cameron Smith <cameron.ray.smith@gmail.com>
2022-11-19 20:38:24 +0000

    refactor postprocessing and figure gen

--- reproducibility/figures/fig2/fig2_pancreas_data.py
+++ reproducibility/figures/fig2/fig2_pancreas_data.py
@@ -16,6 +16,7 @@ from pyrovelocity.plot import vector_field_uncertainty
 
 Inputs:
   "data/Pancreas/endocrinogenesis_day15.h5ad" via load_data()
+  "pancreas_scvelo_fitted_2000_30.h5ad" via load_data()
 
 Outputs:
   data:
@@ -28,6 +29,7 @@ Outputs:
     ├── param_store_test.pt
     └── var_names.csv
   figures:
+    "fig2_test_sub.pdf"
     "fig2_test_volcano_sub.pdf"
     "fig2_test_rainbow_sub.pdf"
     "fig2_test_vecfield_sub.pdf"
@@ -66,47 +68,75 @@ v_map_all, embeds_radian, fdri = vector_field_uncertainty(
     adata, adata_model_pos[1], basis="umap"
 )
 
-##################
-# generate figures
-##################
-
+#############
+# postprocess
+#############
 
 def check_shared_time(adata_model_pos, adata):
     adata.obs["cell_time"] = adata_model_pos[1]["cell_time"].squeeze().mean(0)
     adata.obs["1-Cytotrace"] = 1 - adata.obs["cytotrace"]
-    fig, ax = plt.subplots(1, 3)
-    fig.set_size_inches(15, 3)
-    scv.pl.scatter(
-        adata,
-        color="latent_time",
-        show=False,
-        ax=ax[0],
-        title="scvelo %.2f"
-        % spearmanr(1 - adata.obs.cytotrace, adata.obs.latent_time)[0],
-        cmap="RdBu_r",
-        basis="umap",
-    )
-    scv.pl.scatter(
-        adata,
-        color="cell_time",
-        show=False,
-        basis="umap",
-        ax=ax[1],
-        title="pyro %.2f" % spearmanr(1 - adata.obs.cytotrace, adata.obs.cell_time)[0],
-    )
-    scv.pl.scatter(adata, color="1-Cytotrace", show=False, ax=ax[2])
-    print(spearmanr(adata.obs.cytotrace, adata.obs.cell_time))
-    print(spearmanr(adata.obs.cell_time, adata.obs.latent_time))
-    fig.savefig(
-        "fig2_test_sub.pdf",
-        facecolor=fig.get_facecolor(),
-        bbox_inches="tight",
-        edgecolor="none",
-        dpi=300,
-    )
 
 
 check_shared_time(adata_model_pos, adata)
+
+##################
+# save checkpoints
+##################
+
+adata.write("fig2_pancreas_processed.h5ad")
+
+adata_model_pos[0].save("Fig2_pancreas_model", overwrite=True)
+result_dict = {
+    "adata_model_pos": adata_model_pos[1],
+    "v_map_all": v_map_all,
+    "embeds_radian": embeds_radian,
+    "fdri": fdri,
+    "embed_mean": embed_mean,
+}
+
+
+with open("fig2_pancreas_data.pkl", "wb") as f:
+    pickle.dump(result_dict, f)
+
+
+##################
+# generate figures
+##################
+
+# check shared time plot
+
+fig, ax = plt.subplots(1, 3)
+fig.set_size_inches(15, 3)
+scv.pl.scatter(
+    adata,
+    color="latent_time",
+    show=False,
+    ax=ax[0],
+    title="scvelo %.2f"
+    % spearmanr(1 - adata.obs.cytotrace, adata.obs.latent_time)[0],
+    cmap="RdBu_r",
+    basis="umap",
+)
+scv.pl.scatter(
+    adata,
+    color="cell_time",
+    show=False,
+    basis="umap",
+    ax=ax[1],
+    title="pyro %.2f" % spearmanr(1 - adata.obs.cytotrace, adata.obs.cell_time)[0],
+)
+scv.pl.scatter(adata, color="1-Cytotrace", show=False, ax=ax[2])
+print(spearmanr(adata.obs.cytotrace, adata.obs.cell_time))
+print(spearmanr(adata.obs.cell_time, adata.obs.latent_time))
+fig.savefig(
+    "fig2_test_sub.pdf",
+    facecolor=fig.get_facecolor(),
+    bbox_inches="tight",
+    edgecolor="none",
+    dpi=300,
+)
+
+# plot gene ranking
 
 fig, ax = plt.subplots()
 volcano_data, _ = plot_gene_ranking(
@@ -119,6 +149,8 @@ fig.savefig(
     edgecolor="none",
     dpi=300,
 )
+
+# rainbow plot
 
 print(
     volcano_data.sort_values("mean_mae", ascending=False)
@@ -144,6 +176,8 @@ fig.savefig(
     dpi=300,
 )
 
+# mean vector field plot
+
 fig, ax = plt.subplots()
 embed_mean = plot_mean_vector_field(adata_model_pos[1], adata, ax=ax)
 fig.savefig(
@@ -154,22 +188,3 @@ fig.savefig(
     dpi=300,
 )
 
-
-##################
-# save checkpoints
-##################
-
-adata.write("fig2_pancreas_processed.h5ad")
-
-adata_model_pos[0].save("Fig2_pancreas_model", overwrite=True)
-result_dict = {
-    "adata_model_pos": adata_model_pos[1],
-    "v_map_all": v_map_all,
-    "embeds_radian": embeds_radian,
-    "fdri": fdri,
-    "embed_mean": embed_mean,
-}
-
-
-with open("fig2_pancreas_data.pkl", "wb") as f:
-    pickle.dump(result_dict, f)
