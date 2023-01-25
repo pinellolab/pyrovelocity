2671f8f3f599851202177a6b8fc895a7107c2052
Cameron Smith <cameron.ray.smith@gmail.com>
2023-01-24 22:22:20 -0500

    update conda lock generation script
    * conda-lock 1.4.0

--- conda/gen-conda-lock.sh
+++ conda/gen-conda-lock.sh
@@ -11,14 +11,16 @@ set -x
 #
 # you can recreate an environment from a conda lock file with:
 #
-#   conda create -n <env> --file <lockfile>
-#
-# for example
-#
-# conda create --name YOURENV --file conda-linux-64-cpu.lock
+#   conda-lock --conda mamba install -n YOURENV --file conda-lock.yml
 
-conda-lock --kind explicit -f environment-cpu.yml
-mv conda-linux-64.lock conda-linux-64-cpu.lock
+# DEPRECATED
+# conda-lock --kind explicit -f environment-cpu.yml
+# mv conda-linux-64.lock conda-linux-64-cpu.lock
+# conda-lock --kind explicit -f environment-gpu.yml
+# mv conda-linux-64.lock conda-linux-64-gpu.lock
 
-conda-lock --kind explicit -f environment-gpu.yml
-mv conda-linux-64.lock conda-linux-64-gpu.lock
+# UPDATED
+# note that virtual packages are derived from ./virtual-packages.yml
+# (re)move to disable
+conda-lock --conda mamba --log-level DEBUG -f environment-cpu.yml --lockfile conda-lock-cpu.yml
+conda-lock --conda mamba --log-level DEBUG -f environment-gpu.yml --lockfile conda-lock-gpu.yml
