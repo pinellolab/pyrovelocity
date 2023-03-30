25e68c49802eed6fa77a229304f622c957247fdf
Cameron Smith <cameron.ray.smith@gmail.com>
2023-03-30 04:37:35 +0000

    remove `pyrovelocity.config.config_setup`
    - was overwriting config file

--- reproducibility/figures/fig2/fig2_pancreas_plot.py
+++ reproducibility/figures/fig2/fig2_pancreas_plot.py
@@ -4,12 +4,14 @@ import pickle
 from logging import Logger
 from pathlib import Path
 
+import hydra
 import matplotlib.pyplot as plt
 import scvelo as scv
 from omegaconf import DictConfig
 from scipy.stats import spearmanr
 
-from pyrovelocity.config import config_setup
+# from pyrovelocity.config import config_setup
+from pyrovelocity.config import print_config_tree
 from pyrovelocity.plot import plot_gene_ranking
 from pyrovelocity.plot import us_rainbowplot
 from pyrovelocity.utils import get_pylogger
@@ -172,14 +174,16 @@ def plots(conf: DictConfig, logger: Logger) -> None:
         )
 
 
-def main(config_path: str) -> None:
+@hydra.main(version_base="1.2", config_path="..", config_name="config.yaml")
+def main(conf: DictConfig) -> None:
     """Plot results
     Args:
-        config_path {Text}: path to config
+        conf {DictConfig}: hydra configuration
     """
-    conf = config_setup(config_path)
 
     logger = get_pylogger(name="PLOT", log_level=conf.base.log_level)
+    print_config_tree(conf, logger, ())
+
     logger.info(
         f"\n\nVerifying existence of paths for:\n\n"
         f"  model data: {conf.model_training.pancreas_model1.path}\n"
@@ -192,8 +196,4 @@ def main(config_path: str) -> None:
 
 
 if __name__ == "__main__":
-    args_parser = argparse.ArgumentParser()
-    args_parser.add_argument("--config", dest="config", required=True)
-    args = args_parser.parse_args()
-
-    main(config_path=args.config)
+    main()
