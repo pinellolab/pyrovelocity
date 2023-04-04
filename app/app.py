7ac097d22acb88745a26aa2413bf147782365a5a
Cameron Smith <cameron.ray.smith@gmail.com>
2023-04-03 23:55:49 -0400

    refactor app
    * thanks to https://github.com/sicara/pycon-2022-dvc-streamlit/tree/39109b51cabc8de4644a709a157baf1b9f955d63

--- app/app.py
+++ app/app.py
@@ -1,14 +1,23 @@
+from pathlib import Path
+
 import altair as alt
 import scvelo as scv
 import streamlit as st
-from google.cloud import storage
+from pages import home
+from pages import simulation
+from utils.html_factory import CSSStyle
+from utils.html_factory import make_div
+from utils.html_factory import make_img
+from utils.html_factory import st_write_bs4
+
+from pyrovelocity.config import initialize_hydra_config
 
 
 # setup
 st.set_page_config(
     page_title="pyrovelocity report",
     page_icon="https://raw.githubusercontent.com/pinellolab/pyrovelocity/master/docs/_static/logo.png",
-    layout="centered",
+    layout="wide",
     initial_sidebar_state="auto",
 )
 
@@ -25,38 +34,62 @@ streamlit_style = """
 			}
             footer {visibility: hidden;}
             header {visibility: hidden;}
-            div.block-container{padding-top:0rem;}
+            div.block-container{padding-top:2rem;}
             </style>
             """
 st.markdown(streamlit_style, unsafe_allow_html=True)
 
-st.title("pyrovelocity report")
+# initialize hydra configuration
+cfg = initialize_hydra_config()
 
-# https://bit.ly/42WbSYD
-# http://storage.googleapis.com/BUCKET_NAME/OBJECT_NAME
-@st.cache_data(show_spinner=False)
-def load_gcs_data(bucket_name, source_path, destination_file_name):
-    client = storage.Client.create_anonymous_client()
-    bucket = client.bucket(bucket_name=bucket_name)
-    blob = bucket.blob(source_path)
-    blob.download_to_filename(destination_file_name)
+sidebar_header_logo = make_img(
+    src=Path("./docs/_static/logo.png"),
+    style=CSSStyle(width="128px", flex=1, margin_right="20px"),
+)
+sidebar_header_title = make_div(style=CSSStyle(text_align="left", flex=3))
+sidebar_header_title.extend(
+    [
+        make_div(
+            style=CSSStyle(font_size="18px", font_weight="bold"),
+            text="pyrovelocity",
+        ),
+        make_div(
+            style=CSSStyle(font_size="14px"),
+            text="Probabilistic model of",
+        ),
+        make_div(
+            style=CSSStyle(font_size="14px", font_style="italic"),
+            text="RNA velocity",
+        ),
+    ]
+)
+sidebar_header = make_div(
+    style=CSSStyle(margin_top="-80px", margin_bottom="20px", display="flex")
+)
+sidebar_header.extend([sidebar_header_logo, sidebar_header_title])
 
-    print(
-        f"\nDownloaded blob {source_path}\n"
-        f"from bucket {bucket.name}\n"
-        f"to {destination_file_name}.\n"
-    )
+with st.sidebar:
+    st_write_bs4(sidebar_header)
 
+# Main Chapter selection
+no_data = "🪹 home"
+sidebar_label = "NAVIGATE"
 
-data_set_name = "pancreas"
-model_information = dict(model_name="fixed time model", model_id="model1")
-data_file_name = f"{data_set_name}_{model_information['model_id']}.h5ad"
+selected_page = st.sidebar.selectbox(
+    label=sidebar_label,
+    options=[
+        no_data,
+        "⚪️ pancreas",
+        "🟠 pons",
+        "🔴 peripheral blood",
+        "🔵 hematopoietic stem cells",
+        "🖥️ simulation",
+    ],
+)
 
-with st.spinner(
-    f"loading {data_set_name} data for {model_information['model_name']}..."
-):
-    load_gcs_data(
-        "pyrovelocity",
-        f"data/{data_set_name}/{model_information['model_id']}/trained.h5ad",
-        data_file_name,
-    )
+if selected_page == no_data:
+    home.st_show()
+elif selected_page == "🖥️ simulation":
+    simulation.st_show()
+else:
+    st.error("Page does not yet exist.")
