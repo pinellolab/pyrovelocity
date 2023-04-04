a5544e50084e267348a29d57abeb3d165c19c6c1
Cameron Smith <cameron.ray.smith@gmail.com>
2023-04-04 11:53:16 -0400

    add default altair plotting theme

---
+++ app/utils/altair_theme.py
@@ -0,0 +1,25 @@
+import altair as alt
+
+# define a custom altair theme
+def custom_theme():
+
+    font = "LMSans10"
+    
+    return {
+        "config": {
+            "title": {"fontSize": 16, "font": font},
+            "axis": {
+                "titleFontSize": 18,
+                "labelFontSize": 18,
+                "labelFont": font,
+                "titleFont": font,
+            },
+            "header": {"labelFontSize": 14, "labelFont": font},
+            "legend": {"titleFontSize": 14, "labelFontSize": 12, "font": font},
+             "mark": {
+                 "fontSize": 14,
+                 "tooltip": {"content": "encoding"},  # enable tooltips
+                 "font": font,
+            },
+        }
+    }
