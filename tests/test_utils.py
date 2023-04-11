29de908e96d4a0d5a4f3878facf11520df222ff2
Cameron Smith <cameron.ray.smith@gmail.com>
2023-04-11 22:05:38 +0000

    add `test_generate_sample_data_dimensions`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -14,3 +14,12 @@ def test_load_utils():
 def sample_data():
     return generate_sample_data(random_seed=42)
 
+
+@pytest.mark.parametrize("n_obs, n_vars", [(100, 12), (50, 10), (200, 20)])
+@pytest.mark.parametrize("noise_model", ["iid", "gillespie", "normal"])
+def test_generate_sample_data_dimensions(n_obs, n_vars, noise_model):
+    adata = generate_sample_data(
+        n_obs=n_obs, n_vars=n_vars, noise_model=noise_model, random_seed=42
+    )
+    assert adata.shape == (n_obs, n_vars)
+
