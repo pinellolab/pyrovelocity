a9899af8f9685cbf5b3e83f68626e7b305c396a3
Cameron Smith <cameron.ray.smith@gmail.com>
2023-04-11 22:07:18 +0000

    change test seed to 98

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,14 +12,14 @@ def test_load_utils():
 
 @pytest.fixture
 def sample_data():
-    return generate_sample_data(random_seed=42)
+    return generate_sample_data(random_seed=98)
 
 
 @pytest.mark.parametrize("n_obs, n_vars", [(100, 12), (50, 10), (200, 20)])
 @pytest.mark.parametrize("noise_model", ["iid", "gillespie", "normal"])
 def test_generate_sample_data_dimensions(n_obs, n_vars, noise_model):
     adata = generate_sample_data(
-        n_obs=n_obs, n_vars=n_vars, noise_model=noise_model, random_seed=42
+        n_obs=n_obs, n_vars=n_vars, noise_model=noise_model, random_seed=98
     )
     assert adata.shape == (n_obs, n_vars)
 
@@ -30,8 +30,8 @@ def test_generate_sample_data_layers(sample_data):
 
 
 def test_generate_sample_data_reproducibility():
-    adata1 = generate_sample_data(random_seed=42)
-    adata2 = generate_sample_data(random_seed=42)
+    adata1 = generate_sample_data(random_seed=98)
+    adata2 = generate_sample_data(random_seed=98)
     assert (adata1.X == adata2.X).all()
     assert (adata1.layers["spliced"] == adata2.layers["spliced"]).all()
     assert (adata1.layers["unspliced"] == adata2.layers["unspliced"]).all()
