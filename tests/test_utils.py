eb2aab9c3d5126fa940238b0c710e62fd855af81
Cameron Smith <cameron.ray.smith@gmail.com>
2023-04-11 22:02:18 +0000

    add test for generate_sample_data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,16 @@
 """Tests for `pyrovelocity.utils` module."""
 
+import pytest
+from pyrovelocity.utils import generate_sample_data
+
 
 def test_load_utils():
     from pyrovelocity import utils
 
     print(utils.__file__)
+
+
+@pytest.fixture
+def sample_data():
+    return generate_sample_data(random_seed=42)
+
