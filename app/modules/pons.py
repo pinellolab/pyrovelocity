cdcc2834ec1d391198735a835003ac7db7901e9a
Cameron Smith <cameron.ray.smith@gmail.com>
2023-04-23 16:45:51 -0400

    add pons data

---
+++ app/modules/pons.py
@@ -0,0 +1,18 @@
+import streamlit_book as stb
+
+
+def st_show():
+    stb.set_book_config(
+        menu_title="",
+        menu_icon="",
+        options=[
+            "data review",
+        ],
+        paths=[
+            "app/modules/pons/datareview.py",
+        ],
+        icons=[""],
+        save_answers=False,
+        orientation="vertical",
+        styles={"nav-link": {"font-family": "LMSans10"}},
+    )
