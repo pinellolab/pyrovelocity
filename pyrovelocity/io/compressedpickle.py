5bcf5b930cf56004b9634572c9bf37104041df12
Cameron Smith <cameron.ray.smith@gmail.com>
2023-04-23 21:07:51 -0400

    implement CompressedPickle.save

--- pyrovelocity/io/compressedpickle.py
+++ pyrovelocity/io/compressedpickle.py
@@ -14,3 +14,23 @@ class CompressedPickle:
     >>> loaded_data.equals(test_data)
     True
     """
+
+    @staticmethod
+    def save(file_path: str, obj: Any) -> None:
+        """
+        Save the given object to a zstandard-compressed pickle file.
+        
+        Args:
+            file_path (str): The path of the file to save the object to.
+            obj (object): The object to be saved.
+            
+        Examples:
+        >>> import pandas as pd
+        >>> test_data = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
+        >>> CompressedPickle.save('test_data.pkl.zst', test_data)
+        """
+        with open(file_path, 'wb') as f:
+            compression_context = zstd.ZstdCompressor(level=3)
+            with compression_context.stream_writer(f) as compressor:
+                pickle.dump(obj, compressor)
+
