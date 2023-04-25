0f8742451df0b632422e3f7b4065419d46e2d11d
Cameron Smith <cameron.ray.smith@gmail.com>
2023-04-24 22:34:15 -0400

    additional tests for calculate_criticality_index

--- tests/metrics/test_criticality_index.py
+++ tests/metrics/test_criticality_index.py
@@ -1,6 +1,10 @@
+import anndata
+import numpy as np
 import pytest
 import scvelo as scv
+
 from pyrovelocity.metrics.criticality_index import calculate_criticality_index
+
 
 @pytest.fixture(scope="module")
 def adata_fixture():
@@ -16,8 +20,28 @@ def adata_fixture():
         noise_model="gillespie",
     )
 
+
 def test_calculate_criticality_index(adata_fixture):
     criticality_index, _, _, _ = calculate_criticality_index(adata_fixture)
     assert isinstance(criticality_index, float)
     assert 0 <= criticality_index <= 1
 
+
+def test_empty_anndata():
+    empty_adata = anndata.AnnData(X=np.empty((0, 0)))
+    with pytest.raises(ValueError):
+        calculate_criticality_index(empty_adata)
+
+
+def test_missing_unspliced_layer(adata_fixture):
+    adata_no_unspliced = adata_fixture.copy()
+    del adata_no_unspliced.layers["unspliced"]
+    with pytest.raises(KeyError):
+        calculate_criticality_index(adata_no_unspliced)
+
+
+def test_missing_spliced_layer(adata_fixture):
+    adata_no_spliced = adata_fixture.copy()
+    del adata_no_spliced.layers["spliced"]
+    with pytest.raises(KeyError):
+        calculate_criticality_index(adata_no_spliced)
