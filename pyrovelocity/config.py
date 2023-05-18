ede6d2a909685e6bf6ec40cd6eccee3d9795b867
Cameron Smith <cameron.ray.smith@gmail.com>
2023-05-18 04:17:13 +0000

    WIP: config.py for parallel pl

--- pyrovelocity/config.py
+++ pyrovelocity/config.py
@@ -441,6 +441,448 @@ def hydra_zen_configure():
     return config
 
 
+def hydra_zen_compressed_configure():
+    """
+    Use hydra-zen to generate a configuration for hydra's config store.
+    Generates a hydra-zen configuration for the pyrovelocity pipeline,
+    which includes dataset, model training, and reporting configurations.
+
+    This function defines helper functions for creating dataset, model,
+    and reports configurations, and uses these functions to generate the
+    hydra-zen configuration.
+
+    Returns:
+        Config : Type[DataClass]
+        See the documentation for hydra_zen.make_config for more information.
+    """
+    # define helper functions
+    pbuilds = make_custom_builds_fn(zen_partial=True, populate_full_signature=True)
+
+    def create_dataset_config(
+        source, name, dl_root, data_file, rel_path, url, process_method, process_args
+    ):
+        return dict(
+            source=source,
+            data_file=data_file,
+            dl_root=dl_root,
+            dl_path="${.dl_root}/${.data_file}",
+            rel_path=rel_path,
+            url=url,
+            derived=dict(
+                process_method=process_method,
+                process_args=process_args,
+                rel_path="${paths.data}/processed/" + f"{name}_processed.h5ad",
+            ),
+        )
+
+    def create_model_config(
+        source, name, model_suffix, vector_field_basis, **custom_training_parameters
+    ):
+        return dict(
+            path="${paths.models}/" + f"{name}_model{model_suffix}",
+            model_path="${.path}/model",
+            # input_data_path="${data_external."
+            # + f"{source}.{name}"
+            # + ".derived.rel_path}",
+            input_data_path="${data_sets."
+            + f"{name}"
+            + ".derived.rel_path}",
+            trained_data_path="${.path}/trained.h5ad",
+            pyrovelocity_data_path="${.path}/pyrovelocity.pkl",
+            metrics_path="${.path}/metrics.json",
+            run_info_path="${.path}/run_info.json",
+            vector_field_parameters=dict(basis=vector_field_basis),
+            training_parameters=pbuilds(
+                train_model,
+                loss_plot_path="${..path}/loss_plot.png",
+                patient_improve=0.0001,
+                patient_init=45,
+                **custom_training_parameters,
+            ),
+        )
+
+    def create_reports_config(model_name: str, model_number: int):
+        path = f"{paths['reports']}/{model_name}_model{model_number}"
+        return dict(
+            path=path,
+            dataframe_path=f"{paths['data']}/processed/{model_name}_model{model_number}_dataframe.pkl.zst",
+            shared_time_plot=f"{path}/shared_time.pdf",
+            volcano_plot=f"{path}/volcano.pdf",
+            rainbow_plot=f"{path}/rainbow.pdf",
+            uncertainty_param_plot=f"{path}/param_uncertainties.pdf",
+            vector_field_plot=f"{path}/vector_field.pdf",
+            biomarker_selection_plot=f"{path}/markers_selection_scatterplot.tif",
+            biomarker_phaseportrait_plot=f"{path}/markers_phaseportrait.pdf",
+        )
+
+    # define configuration
+    base = dict(log_level="INFO")
+
+    paths = dict(
+        data="data",
+        models="models",
+        reports="reports",
+    )
+
+    process_data = [
+        "simulate_medium",
+        "pons",
+        "pancreas",
+        "pbmc10k",
+        "pbmc68k",
+        "larry",
+        "larry_tips",
+        "larry_mono",
+        "larry_neu",
+        "larry_multilineage",
+    ]
+
+    train_models=[
+        "simulate_medium_model1",
+        "simulate_medium_model2",
+        "pancreas_model1",
+        "pancreas_model2",
+        "pbmc68k_model1",
+        "pbmc68k_model2",
+        "pons_model1",
+        "pons_model2",
+        "larry_model2",
+        "larry_tips_model2",
+        "larry_mono_model2",
+        "larry_neu_model2",
+        "larry_multilineage_model2",
+        "pbmc10k_model2",
+    ]
+
+    return make_config(
+        base=base,
+        paths=paths,
+        process_data=process_data,
+        train_models=train_models,
+        data_sets=dict(
+            # root_path="${paths.data}/external",
+            # processed_path="${paths.data}/processed",
+            # sources=["simulate", "velocyto", "scvelo", "pyrovelocity"],
+            simulate_medium=create_dataset_config(
+                    source="simulate",
+                    name="simulated_medium",
+                    dl_root="${paths.data}/external",
+                    data_file="simulated_medium.h5ad",
+                    rel_path="${.dl_root}/simulated_medium.h5ad",
+                    url="https://storage.googleapis.com/pyrovelocity/data/simulated_medium.h5ad",
+                    process_method="load_data",
+                    process_args=dict(),
+                ),
+            # velocyto=dict(
+            #     download=["pons"],
+            #     process=["pons"],
+            #     sources=dict(
+            #         gcs_root_url="https://storage.googleapis.com/pyrovelocity/data"
+            #     ),
+                pons=create_dataset_config(
+                    source="velocyto",
+                    name="pons",
+                    dl_root="${paths.data}/external",
+                    data_file="oligo_lite.h5ad",
+                    rel_path="${.dl_root}/oligo_lite.h5ad",
+                    url="https://storage.googleapis.com/pyrovelocity/data/oligo_lite.h5ad",
+                    process_method="load_data",
+                    process_args=dict(),
+                ),
+            # ),
+            # scvelo=dict(
+            #     download=["pancreas", "pbmc68k"],
+            #     process=["pancreas", "pbmc68k"],
+            #     sources=dict(
+            #         figshare_root_url="https://ndownloader.figshare.com/files",
+            #         scvelo_root_url="https://github.com/theislab/scvelo_notebooks/raw/master",
+            #     ),
+                pancreas=create_dataset_config(
+                    source="scvelo",
+                    name="pancreas",
+                    dl_root="data/Pancreas",
+                    data_file="endocrinogenesis_day15.h5ad",
+                    rel_path="${paths.data}/external/endocrinogenesis_day15.h5ad",
+                    url="https://github.com/theislab/scvelo_notebooks/raw/master/data/Pancreas/endocrinogenesis_day15.h5ad",
+                    process_method="load_data",
+                    process_args=dict(process_cytotrace=True),
+                ),
+                pbmc68k=create_dataset_config(
+                    source="scvelo",
+                    name="pbmc68k",
+                    dl_root="data/PBMC",
+                    data_file="pbmc68k.h5ad",
+                    rel_path="${paths.data}/external/pbmc68k.h5ad",
+                    url="https://ndownloader.figshare.com/files/27686886",
+                    process_method="load_data",
+                    process_args=dict(),
+                ),
+            # ),
+            # pyrovelocity=dict(
+            #     download=["larry", "pbmc10k"],
+            #     process=["pbmc10k"],
+            #     sources=dict(
+            #         figshare_root_url="https://ndownloader.figshare.com/files"
+            #     ),
+                larry=create_dataset_config(
+                    source="pyrovelocity",
+                    name="larry",
+                    dl_root="${paths.data}/external",
+                    data_file="larry.h5ad",
+                    rel_path="${.dl_root}/larry.h5ad",
+                    url="https://ndownloader.figshare.com/files/37028569",
+                    process_method="load_data",
+                    process_args=dict(),
+                ),
+                larry_tips=create_dataset_config(
+                    source="pyrovelocity",
+                    name="larry_tips",
+                    dl_root="${paths.data}/external",
+                    data_file="larry_tips.h5ad",
+                    rel_path="${.dl_root}/larry_tips.h5ad",
+                    url="https://storage.googleapis.com/pyrovelocity/data/larry_tips.h5ad",
+                    process_method="load_data",
+                    process_args=dict(),
+                ),
+                larry_mono=create_dataset_config(
+                    source="pyrovelocity",
+                    name="larry_mono",
+                    dl_root="${paths.data}/external",
+                    data_file="larry_mono.h5ad",
+                    rel_path="${.dl_root}/larry_mono.h5ad",
+                    url="https://storage.googleapis.com/pyrovelocity/data/larry_mono.h5ad",
+                    process_method="load_data",
+                    process_args=dict(),
+                ),
+                larry_neu=create_dataset_config(
+                    source="pyrovelocity",
+                    name="larry_neu",
+                    dl_root="${paths.data}/external",
+                    data_file="larry_neu.h5ad",
+                    rel_path="${.dl_root}/larry_neu.h5ad",
+                    url="https://storage.googleapis.com/pyrovelocity/data/larry_neu.h5ad",
+                    process_method="load_data",
+                    process_args=dict(),
+                ),
+                larry_multilineage=create_dataset_config(
+                    source="pyrovelocity",
+                    name="larry_multilineage",
+                    dl_root="${paths.data}/external",
+                    data_file="larry_multilineage.h5ad",
+                    rel_path="${.dl_root}/larry_multilineage.h5ad",
+                    url="https://storage.googleapis.com/pyrovelocity/data/larry_multilineage.h5ad",
+                    process_method="load_data",
+                    process_args=dict(),
+                ),
+                pbmc10k=create_dataset_config(
+                    source="pyrovelocity",
+                    name="pbmc10k",
+                    dl_root="${paths.data}/external",
+                    data_file="pbmc10k.h5ad",
+                    rel_path="${.dl_root}/pbmc10k.h5ad",
+                    url="https://storage.googleapis.com/pyrovelocity/data/pbmc10k.h5ad",
+                    process_method="load_data",
+                    process_args=dict(),
+                ),
+            # ),
+        ),
+        model_training=dict(
+            simulate_model1=create_model_config(
+                "simulate",
+                "simulate_medium",
+                1,
+                "umap",
+                cell_state="leiden",
+                guide_type="auto_t0_constraint",
+                max_epochs=4000,
+            ),
+            simulate_model2=create_model_config(
+                "simulate",
+                "simulate_medium",
+                2,
+                "umap",
+                cell_state="leiden",
+                max_epochs=4000,
+                offset=True,
+            ),
+            pancreas_model1=create_model_config(
+                "scvelo",
+                "pancreas",
+                1,
+                "umap",
+                guide_type="auto_t0_constraint",
+                max_epochs=2000,
+            ),
+            pancreas_model2=create_model_config(
+                "scvelo",
+                "pancreas",
+                2,
+                "umap",
+                offset=True,
+                max_epochs=2000,
+            ),
+            pbmc68k_model1=create_model_config(
+                "scvelo",
+                "pbmc68k",
+                1,
+                "tsne",
+                guide_type="auto_t0_constraint",
+                cell_state="celltype",
+                max_epochs=2000,
+            ),
+            pbmc68k_model2=create_model_config(
+                "scvelo",
+                "pbmc68k",
+                2,
+                "tsne",
+                cell_state="celltype",
+                offset=True,
+                max_epochs=2000,
+            ),
+            pons_model1=create_model_config(
+                "velocyto",
+                "pons",
+                1,
+                "umap",
+                guide_type="auto_t0_constraint",
+                cell_state="celltype",
+                max_epochs=2000,
+            ),
+            pons_model2=create_model_config(
+                "velocyto",
+                "pons",
+                2,
+                "umap",
+                cell_state="celltype",
+                offset=True,
+                max_epochs=2000,
+            ),
+            larry_model2=create_model_config(
+                "pyrovelocity",
+                "larry",
+                2,
+                "emb",
+                svi_train=True,
+                batch_size=4000,
+                cell_state="state_info",
+                offset=True,
+                max_epochs=1000,
+            ),
+            larry_tips_model2=create_model_config(
+                "pyrovelocity",
+                "larry_tips",
+                2,
+                "umap",
+                svi_train=True,
+                batch_size=4000,
+                cell_state="state_info",
+                offset=True,
+                max_epochs=1000,
+            ),
+            larry_mono_model2=create_model_config(
+                "pyrovelocity",
+                "larry_mono",
+                2,
+                "emb",
+                svi_train=True,
+                batch_size=4000,
+                cell_state="state_info",
+                offset=True,
+                max_epochs=1000,
+            ),
+            larry_neu_model2=create_model_config(
+                "pyrovelocity",
+                "larry_neu",
+                2,
+                "emb",
+                svi_train=True,
+                batch_size=4000,
+                cell_state="state_info",
+                offset=True,
+                max_epochs=1000,
+            ),
+            larry_multilineage_model2=create_model_config(
+                "pyrovelocity",
+                "larry_multilineage",
+                2,
+                "emb",
+                svi_train=True,
+                batch_size=4000,
+                cell_state="state_info",
+                offset=True,
+                max_epochs=1000,
+            ),
+            pbmc10k_model2=create_model_config(
+                "pyrovelocity",
+                "pbmc10k",
+                2,
+                "tsne",
+                cell_state="leiden",
+                offset=True,
+                max_epochs=2000,
+            ),
+        ),
+        reports=dict(
+            model_summary=dict(
+                # summarize=[
+                #     "simulate_model1",
+                #     "simulate_model2",
+                #     "pancreas_model1",
+                #     "pancreas_model2",
+                #     "pbmc68k_model1",
+                #     "pbmc68k_model2",
+                #     "pons_model1",
+                #     "pons_model2",
+                #     "pbmc10k_model2",
+                #     "larry_tips_model2",
+                # ],
+                simulate_model1=create_reports_config("medium", 1),
+                simulate_model2=create_reports_config("medium", 2),
+                pancreas_model1=create_reports_config("pancreas", 1),
+                pancreas_model2=create_reports_config("pancreas", 2),
+                pbmc68k_model1=create_reports_config("pbmc68k", 1),
+                pbmc68k_model2=create_reports_config("pbmc68k", 2),
+                pons_model1=create_reports_config("pons", 1),
+                pons_model2=create_reports_config("pons", 2),
+                pbmc10k_model2=create_reports_config("pbmc10k", 2),
+                larry_tips_model2=create_reports_config("larry_tips", 2),
+            ),
+            figure2=dict(
+                tag="fig2",
+                path="${paths.reports}/${.tag}",
+                tif_path="${.path}/${.tag}_raw_gene_selection_model1.tif",
+                svg_path="${.path}/${.tag}_raw_gene_selection_model1.svg",
+                biomarker_selection_path="${.path}/${.tag}_markers_selection_scatterplot.tif",
+                biomarker_phaseportrait_path="${.path}/${.tag}_markers_phaseportrait.pdf",
+                uncertainty_param_plot_path="${.path}/${.tag}_param_uncertainties.pdf",
+                uncertainty_magnitude_plot_path="${.path}/${.tag}_magnitude_uncertainties.pdf",
+                pancreas_model1=dict(
+                    shared_time_plot="${..path}/${..tag}_pancreas_shared_time.pdf",
+                    volcano_plot="${..path}/${..tag}_pancreas_volcano.pdf",
+                    rainbow_plot="${..path}/${..tag}_pancreas_rainbow.pdf",
+                    vector_field_plot="${..path}/${..tag}_pancreas_vector_field.pdf",
+                ),
+            ),
+            figure2_extras=dict(
+                tag="fig2",
+                path="${paths.reports}/${.tag}",
+                shared_time_plot="${.path}/figure2_extras.pdf",
+            ),
+            figureS3=dict(
+                tag="figS3",
+                path="${paths.reports}/${.tag}",
+                tif_path="${.path}/${.tag}_raw_gene_selection_model2.tif",
+                svg_path="${.path}/${.tag}_raw_gene_selection_model2.svg",
+            ),
+            figureS3_extras=dict(
+                tag="figS3",
+                path="${paths.reports}/${.tag}",
+                shared_time_plot="${.path}/figureS3_extras.pdf",
+            ),
+        ),
+    )
+
+
 def test_hydra_zen_configure():
     """
     Use hydra-zen to generate a test configuration for hydra's config store.
