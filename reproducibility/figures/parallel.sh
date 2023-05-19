488eed51a1e2ef0cffcd7b75e33208aafa7af6f3
Cameron Smith <cameron.ray.smith@gmail.com>
2023-05-19 12:19:32 +0000

    manually specify model training order

--- reproducibility/figures/parallel.sh
+++ reproducibility/figures/parallel.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 
 dvc stage list --name-only |\
     grep -E "data_download*" |\
-    xargs -t -n 1 -P 5 bash -c 'sleep $((RANDOM % 6 + 3)); dvc repro "$@"' --
+    xargs -t -n 1 -P 4 bash -c 'sleep $((RANDOM % 6 + 3)); dvc repro "$@"' --
 
 wait
 
@@ -15,9 +15,26 @@ dvc stage list --name-only |\
 
 wait
 
-dvc stage list --name-only |\
-    grep -E "train*" |\
-    xargs -t -n 1 -P 4 bash -c 'sleep $((RANDOM % 6 + 3)); dvc repro "$@"' --
+# dvc stage list --name-only |\
+#     grep -E "train*" |\
+#     xargs -t -n 1 -P 4 bash -c 'sleep $((RANDOM % 6 + 3)); dvc repro "$@"' --
+dvc repro train@pancreas_model2 &
+sleep 2
+dvc repro train@pbmc68k_model2 &
+sleep 2
+dvc repro train@pons_model2 &
+sleep 2
+dvc repro train@larry_model2 &
+wait
+dvc repro train@larry_tips_model2 &
+sleep 2
+dvc repro train@larry_mono_model2 &
+sleep 2
+dvc repro train@larry_neu_model2 &
+sleep 2
+dvc repro train@larry_multilineage_model2 &
+wait
+dvc repro train@pbmc10k_model2 &
 
 wait
 
