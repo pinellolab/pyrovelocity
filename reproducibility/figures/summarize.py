fffc525f6a19f3cd707f2aea03392cb793e34486
Cameron Smith <cameron.ray.smith@gmail.com>
2023-05-26 06:26:53 +0000

    update parameter uncertainty violin plots

--- reproducibility/figures/summarize.py
+++ reproducibility/figures/summarize.py
@@ -472,16 +472,25 @@ def plot_parameter_posterior_distributions(
         print(posterior_samples[kinetics].squeeze().shape)
         print(np.isin(adata.var_names, list(geneset)).sum())
         df = pd.DataFrame(
-            posterior_samples[kinetics].squeeze()[
-                :, np.isin(adata.var_names, list(geneset))
-            ],
+            np.log(
+                posterior_samples[kinetics].squeeze()[
+                    :, np.isin(adata.var_names, list(geneset))
+                ],
+            ),
             columns=adata.var_names[np.isin(adata.var_names, list(geneset))],
         )
+        df = df.apply(lambda x: x - x.mean())
         df_long = df.melt()
         print(df_long.head())
-        ax1 = sns.violinplot(x="index", y="value", data=df_long, ax=ax[index])
+        df_long["index"] = pd.Categorical(
+            df_long["index"], categories=geneset, ordered=True
+        )
+        ax1 = sns.violinplot(
+            x="index", y="value", data=df_long, ax=ax[index], showfliers=False
+        )
         ax1.set_xticklabels(ax1.get_xticklabels(), rotation=30, ha="right")
         ax1.set_ylabel(kinetics)
+        ax1.set_xlabel("")
     fig.subplots_adjust(
         hspace=0.4, wspace=0.45, left=0.08, right=0.95, top=0.9, bottom=0.15
     )
