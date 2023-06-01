f504be2ba8065e4991e2ecedd83c91419e019ce6
Cameron Smith <cameron.ray.smith@gmail.com>
2023-06-01 14:02:25 -0400

    lint figS2

--- reproducibility/figures/figS2/figure.py
+++ reproducibility/figures/figS2/figure.py
@@ -17,6 +17,11 @@ from astropy.stats import circstd
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib_venn import venn2
 from omegaconf import DictConfig
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import LabelEncoder
 from statannotations.Annotator import Annotator
 
 from pyrovelocity.config import print_config_tree
@@ -28,11 +33,6 @@ from pyrovelocity.plot import plot_posterior_time
 from pyrovelocity.plot import plot_vector_field_uncertain
 from pyrovelocity.plot import rainbowplot
 from pyrovelocity.utils import get_pylogger
-
-from sklearn.metrics import roc_auc_score
-from sklearn.preprocessing import LabelEncoder, LabelBinarizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import cross_val_score
 
 
 def plots(
@@ -72,8 +72,8 @@ def plots(
         umap_fdri = np.sum(posterior_samples["fdri"] < 0.001)
         pca_fdri = np.sum(posterior_samples["pca_fdri"] < 0.001)
         num_cells = posterior_samples["fdri"].shape[0]
-        rayleigh_umap_angles_test.append(umap_fdri/num_cells)
-        rayleigh_pca_angles_test.append(pca_fdri/num_cells)
+        rayleigh_umap_angles_test.append(umap_fdri / num_cells)
+        rayleigh_pca_angles_test.append(pca_fdri / num_cells)
 
         print(posterior_samples["cell_time"].shape)
         multiclass_macro_aucs_test = []
@@ -91,7 +91,9 @@ def plots(
             # multiclass_macro_aucs_test.append(macro_roc_auc_ovr)
 
             # cross validation test
-            macro_roc_auc_ovr_crossval = cross_val_score(lr, x_all, y_sample, cv=5, scoring='f1_macro')
+            macro_roc_auc_ovr_crossval = cross_val_score(
+                lr, x_all, y_sample, cv=5, scoring="f1_macro"
+            )
             multiclass_macro_aucs_test.append(np.mean(macro_roc_auc_ovr_crossval))
         macro_labels += [data_model] * sample_size
         multiclass_macro_aucs_test_all_models += multiclass_macro_aucs_test
@@ -99,17 +101,46 @@ def plots(
     print(len(macro_labels))
     print(len(multiclass_macro_aucs_test_all_models))
     print(multiclass_macro_aucs_test_all_models)
-    global_uncertainties_aucs = pd.DataFrame({"dataset": macro_labels, 'f1_macro': multiclass_macro_aucs_test_all_models})
-    cell_uncertainties_metrics = pd.DataFrame({"dataset": conf.train_models, "rayleigh_umap_angles_test": rayleigh_umap_angles_test, "rayleigh_pca_angles_test":rayleigh_pca_angles_test})
+    global_uncertainties_aucs = pd.DataFrame(
+        {"dataset": macro_labels, "f1_macro": multiclass_macro_aucs_test_all_models}
+    )
+    cell_uncertainties_metrics = pd.DataFrame(
+        {
+            "dataset": conf.train_models,
+            "rayleigh_umap_angles_test": rayleigh_umap_angles_test,
+            "rayleigh_pca_angles_test": rayleigh_pca_angles_test,
+        }
+    )
     fig, ax = plt.subplots(1, 3)
     fig.set_size_inches(15, 3.5)
-    sns.barplot(data=cell_uncertainties_metrics, x='dataset', y='rayleigh_umap_angles_test', ax=ax[0])
-    sns.barplot(data=cell_uncertainties_metrics, x='dataset', y='rayleigh_pca_angles_test', ax=ax[1])
+    sns.barplot(
+        data=cell_uncertainties_metrics,
+        x="dataset",
+        y="rayleigh_umap_angles_test",
+        ax=ax[0],
+    )
+    sns.barplot(
+        data=cell_uncertainties_metrics,
+        x="dataset",
+        y="rayleigh_pca_angles_test",
+        ax=ax[1],
+    )
 
-    print(global_uncertainties_aucs.groupby('dataset').median())
-    grouped_index = global_uncertainties_aucs.groupby('dataset').median().sort_values(by='f1_macro').index
+    print(global_uncertainties_aucs.groupby("dataset").median())
+    grouped_index = (
+        global_uncertainties_aucs.groupby("dataset")
+        .median()
+        .sort_values(by="f1_macro")
+        .index
+    )
     print(grouped_index)
-    sns.boxplot(data=global_uncertainties_aucs, x='dataset', y='f1_macro', ax=ax[2], order=grouped_index)
+    sns.boxplot(
+        data=global_uncertainties_aucs,
+        x="dataset",
+        y="f1_macro",
+        ax=ax[2],
+        order=grouped_index,
+    )
     fig.autofmt_xdate(rotation=45)
     fig.savefig(
         fig_name,
@@ -136,18 +167,16 @@ def main(conf: DictConfig) -> None:
     Path(conf.reports.figureS2.path).mkdir(parents=True, exist_ok=True)
     confS2 = conf.reports.figureS2
 
-    if os.path.isfile(confS2.violin_plots_other_lin) and os.path.isfile(confS2.violin_plots_larry_lin):
+    if os.path.isfile(confS2.violin_plots_other_lin) and os.path.isfile(
+        confS2.violin_plots_larry_lin
+    ):
         logger.info(
             f"\n\nFigure S2 extras outputs already exist:\n\n"
             f"  see contents of: {conf.reports.figureS2_extras.path}\n"
         )
     else:
         for fig_name in [confS2.violin_plots_other_lin]:
-            plots(
-                conf,
-                logger,
-                fig_name=fig_name
-            )
+            plots(conf, logger, fig_name=fig_name)
 
 
 if __name__ == "__main__":
