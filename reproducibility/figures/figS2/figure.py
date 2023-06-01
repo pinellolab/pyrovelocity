5fd9fb3f71a7a46e49b6fbb3373b17f8b25e526f
qinqian <qqin@broadinstitute.org>
2023-06-01 15:59:05 +0000

    add boxplot for macro auc comparison across poterior samples and datasets; tweak cutoff for Rayleigh test

--- reproducibility/figures/figS2/figure.py
+++ reproducibility/figures/figS2/figure.py
@@ -29,6 +29,10 @@ from pyrovelocity.plot import plot_vector_field_uncertain
 from pyrovelocity.plot import rainbowplot
 from pyrovelocity.utils import get_pylogger
 
+from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import LabelEncoder, LabelBinarizer
+from sklearn.linear_model import LogisticRegression
+
 
 def plots(
     conf: DictConfig,
@@ -47,26 +51,54 @@ def plots(
 
     rayleigh_umap_angles_test = []
     rayleigh_pca_angles_test = []
+    multiclass_macro_aucs_test_all_models = []
+    macro_labels = []
+    sample_size = 30
 
     for data_model in conf.train_models:
         ##################
         # load data
         ##################
         print(data_model)
+
         data_model_conf = conf.model_training[data_model]
+        cell_state = data_model_conf.training_parameters.cell_state
+        adata_data_path = data_model_conf.trained_data_path
         pyrovelocity_data_path = data_model_conf.pyrovelocity_data_path
+
+        adata = scv.read(adata_data_path)
         posterior_samples = CompressedPickle.load(pyrovelocity_data_path)
-        umap_fdri = np.sum(posterior_samples["fdri"] < 0.05)
-        pca_fdri = np.sum(posterior_samples["pca_fdri"] < 0.05)
+        umap_fdri = np.sum(posterior_samples["fdri"] < 0.001)
+        pca_fdri = np.sum(posterior_samples["pca_fdri"] < 0.001)
         num_cells = posterior_samples["fdri"].shape[0]
         rayleigh_umap_angles_test.append(umap_fdri/num_cells)
         rayleigh_pca_angles_test.append(pca_fdri/num_cells)
 
+        print(posterior_samples["cell_time"].shape)
+        multiclass_macro_aucs_test = []
+        for sample in range(sample_size):
+            # le = LabelBinarizer()
+            le = LabelEncoder()
+            lr = LogisticRegression()
+            y_sample = le.fit_transform(adata.obs[cell_state].values)
+            x_all = posterior_samples["cell_time"][sample]
+            y_score = lr.fit(x_all, y_sample).predict_proba(x_all)
+
+            # TODO: cross validation
+            macro_roc_auc_ovr = roc_auc_score(y_sample, y_score, multi_class="ovr", average="macro")
+            multiclass_macro_aucs_test.append(macro_roc_auc_ovr)
+        macro_labels += [data_model] * sample_size
+        multiclass_macro_aucs_test_all_models += multiclass_macro_aucs_test
+
+    print(len(macro_labels))
+    print(len(multiclass_macro_aucs_test_all_models))
+    global_uncertainties_aucs = pd.DataFrame({"dataset": macro_labels, 'macro_aucs': multiclass_macro_aucs_test_all_models})
     cell_uncertainties_metrics = pd.DataFrame({"dataset": conf.train_models, "rayleigh_umap_angles_test": rayleigh_umap_angles_test, "rayleigh_pca_angles_test":rayleigh_pca_angles_test})
-    fig, ax = plt.subplots(1, 2)
-    fig.set_size_inches(9.6, 3.5)
+    fig, ax = plt.subplots(1, 3)
+    fig.set_size_inches(15, 3.5)
     sns.barplot(data=cell_uncertainties_metrics, x='dataset', y='rayleigh_umap_angles_test', ax=ax[0])
     sns.barplot(data=cell_uncertainties_metrics, x='dataset', y='rayleigh_pca_angles_test', ax=ax[1])
+    sns.boxplot(data=global_uncertainties_aucs, x='dataset', y='macro_aucs', ax=ax[2])
     fig.autofmt_xdate(rotation=45)
     fig.savefig(
         fig_name,
@@ -92,7 +124,6 @@ def main(conf: DictConfig) -> None:
     )
     Path(conf.reports.figureS2.path).mkdir(parents=True, exist_ok=True)
     confS2 = conf.reports.figureS2
-    print('------test---------')
 
     if os.path.isfile(confS2.violin_plots_other_lin) and os.path.isfile(confS2.violin_plots_larry_lin):
         logger.info(
@@ -100,7 +131,6 @@ def main(conf: DictConfig) -> None:
             f"  see contents of: {conf.reports.figureS2_extras.path}\n"
         )
     else:
-        print('------test---------')
         for fig_name in [confS2.violin_plots_other_lin, confS2.violin_plots_larry_lin]:
             plots(
                 conf,
