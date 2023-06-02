cf66431c8395a2e07d01142329406a214859f5e5
Cameron Smith <cameron.ray.smith@gmail.com>
2023-06-02 20:51:35 +0000

    update fig3

--- reproducibility/figures/fig3/figure.py
+++ reproducibility/figures/fig3/figure.py
@@ -675,15 +675,13 @@ def main(conf: DictConfig) -> None:
 
     logger = get_pylogger(name="PLOT", log_level=conf.base.log_level)
 
-    if os.path.isfile(conf.reports.figureS3.tif_path) and os.path.isfile(
-        conf.reports.figureS3.svg_path
-    ):
+    if os.path.isfile(conf.reports.figure3.figure3):
         logger.info(
-            f"\n\nFigure 2 outputs already exist:\n\n"
-            f"  see contents of: {conf.reports.figureS3.path}\n"
+            f"\n\nFigure 3 outputs already exist:\n\n"
+            f"  see contents of: {conf.reports.figure3.figure3}\n"
         )
     else:
-        logger.info(f"\n\nPlotting figure S3\n\n")
+        logger.info(f"\n\nPlotting figure 3\n\n")
         plots(conf, logger)
 
 
