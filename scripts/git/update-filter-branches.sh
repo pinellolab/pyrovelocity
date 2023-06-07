5a4a35182e60ed417ec8b43e4de2a77e48469c1c
Cameron Smith <cameron.ray.smith@gmail.com>
2023-06-07 18:57:26 +0000

    add script to update filter branches

---
+++ scripts/git/update-filter-branches.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# Update the filter/* branches
+# Afterward:
+#  - gt rs -r
+#  - git checkout --theirs dvc.lock && gt add dvc.lock && gt continue
+
+git fetch upstream
+
+for branch in `git branch -a | grep remotes | grep -v HEAD | grep -E 'upstream/filter/[a-z]+$'`; do
+   echo "Checking out ${branch#remotes/upstream/}"
+   git checkout ${branch#remotes/upstream/}
+   git reset --hard upstream/${branch#remotes/upstream/}
+   gt btr -p master
+done
