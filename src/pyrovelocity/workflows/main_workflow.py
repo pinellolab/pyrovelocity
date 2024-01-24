85869f3f1e48bcdf5ff5892576a4e3cc0d8003cc
Cameron Smith <cameron.ray.smith@gmail.com>
2024-01-23 19:26:58 -0500

    chore(version): 0.2.0b6 (#502) Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/workflows/main_workflow.py
+++ src/pyrovelocity/workflows/main_workflow.py
@@ -33,7 +33,7 @@ DownloadDatasetInterface.__module__ = __name__
 
 @task(
     cache=True,
-    cache_version="0.2.0b5",
+    cache_version="0.2.0b6",
     retries=3,
     interruptible=True,
     timeout=timedelta(minutes=20),
