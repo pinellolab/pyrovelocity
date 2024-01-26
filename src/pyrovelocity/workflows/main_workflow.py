d4f5f89e384ab047ef31c8e476f516c93023d52c
Cameron Smith <cameron.ray.smith@gmail.com>
2024-01-25 22:29:50 -0500

    feat(workflow): add preprocess data tasks Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/workflows/main_workflow.py
+++ src/pyrovelocity/workflows/main_workflow.py
@@ -9,14 +9,17 @@ from mashumaro.mixins.json import DataClassJSONMixin
 
 from pyrovelocity.data import download_dataset
 from pyrovelocity.logging import configure_logging
+from pyrovelocity.preprocess import preprocess_dataset
 from pyrovelocity.workflows.configuration import create_dataclass_from_callable
 
 logger = configure_logging(__name__)
 
+# These can be used to override the default values of the dataclass
+# but are generally only required if the callable interface lacks
+# complete and dataclass-compatible type annotations.
 custom_types_defaults: Dict[str, Tuple[Type, Any]] = {
-    "data_set_name": (str, "pancreas"),
-    "download_file_name": (str, "pancreas"),
-    "source": (str, "scvelo"),
+    # "data_set_name": (str, "pancreas"),
+    # "source": (str, "pyrovelocity"),
 }
 
 download_dataset_fields = create_dataclass_from_callable(
@@ -30,10 +33,12 @@ DownloadDatasetInterface = make_dataclass(
 )
 DownloadDatasetInterface.__module__ = __name__
 
+cache_version = "0.2.0b8"
+
 
 @task(
     cache=True,
-    cache_version="0.2.0b7",
+    cache_version=cache_version,
     retries=3,
     interruptible=True,
     timeout=timedelta(minutes=20),
@@ -43,11 +48,25 @@ def download_data(download_dataset_args: DownloadDatasetInterface) -> FlyteFile:
     """
     Download external data.
     """
-    # import time
-
-    # time.sleep(7200)
     dataset_path = download_dataset(**asdict(download_dataset_args))
     return FlyteFile(path=dataset_path)
+
+
+@task(
+    cache=True,
+    cache_version=cache_version,
+    retries=3,
+    interruptible=True,
+    timeout=timedelta(minutes=20),
+    requests=Resources(cpu="4", mem="8Gi", ephemeral_storage="16Gi"),
+)
+def preprocess_data(data: FlyteFile) -> FlyteFile:
+    """
+    Download external data.
+    """
+    data_path = data.download()
+    processed_dataset_path = preprocess_dataset(data_path=data_path)
+    return FlyteFile(path=processed_dataset_path)
 
 
 @workflow
@@ -58,18 +77,19 @@ def module_workflow(
     Put all of the steps together into a single workflow.
     """
     data = download_data(download_dataset_args=download_dataset_args)
-    return data
+    processed_data = preprocess_data(data=data)
+    # model = train_model(processed_data=processed_data)
+    return processed_data
 
 
 @workflow
-def training_workflow() -> Tuple[FlyteFile, FlyteFile]:
+def training_workflow() -> Tuple[FlyteFile, FlyteFile, FlyteFile, FlyteFile]:
     """
     Apply the module_workflow to all datasets.
     """
     simulated_data = module_workflow(
         download_dataset_args=DownloadDatasetInterface(
             data_set_name="simulated",
-            download_file_name="simulated",
             source="simulate",
         )
     )
@@ -77,11 +97,26 @@ def training_workflow() -> Tuple[FlyteFile, FlyteFile]:
     pancreas_data = module_workflow(
         download_dataset_args=DownloadDatasetInterface(
             data_set_name="pancreas",
-            download_file_name="pancreas",
-            source="scvelo",
         )
     )
-    return (simulated_data, pancreas_data)
+
+    pons_data = module_workflow(
+        download_dataset_args=DownloadDatasetInterface(
+            data_set_name="pons",
+        )
+    )
+
+    pbmc68k_data = module_workflow(
+        download_dataset_args=DownloadDatasetInterface(
+            data_set_name="pbmc68k",
+        )
+    )
+    return (
+        simulated_data,
+        pancreas_data,
+        pons_data,
+        pbmc68k_data,
+    )
 
 
 if __name__ == "__main__":
