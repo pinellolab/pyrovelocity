41c18e27145438eed55c961f521727004ac1f8f3
Cameron Smith <cameron.ray.smith@gmail.com>
2024-01-29 15:10:19 -0500

    feat(workflows): add pbmc68k, pons, and larry data sets Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/workflows/main_workflow.py
+++ src/pyrovelocity/workflows/main_workflow.py
@@ -1,24 +1,21 @@
-from dataclasses import asdict
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 from datetime import timedelta
 
-from flytekit import Resources
-from flytekit import task
-from flytekit import workflow
+from flytekit import Resources, task, workflow
 from flytekit.extras.accelerators import T4
 from flytekit.types.directory import FlyteDirectory
 from flytekit.types.file import FlyteFile
 from mashumaro.mixins.json import DataClassJSONMixin
 
 from pyrovelocity.data import download_dataset
-from pyrovelocity.interfaces import DownloadDatasetInterface
-from pyrovelocity.interfaces import PreprocessDataInterface
-from pyrovelocity.interfaces import PyroVelocityTrainInterface
+from pyrovelocity.interfaces import (
+    DownloadDatasetInterface,
+    PreprocessDataInterface,
+    PyroVelocityTrainInterface,
+)
 from pyrovelocity.logging import configure_logging
 from pyrovelocity.preprocess import preprocess_dataset
-from pyrovelocity.train import PyroVelocityTrainInterface
-from pyrovelocity.train import train_dataset
-
+from pyrovelocity.train import PyroVelocityTrainInterface, train_dataset
 
 logger = configure_logging(__name__)
 
@@ -36,7 +33,7 @@ class TrainingOutputs(DataClassJSONMixin):
 
 
 @task(
-    cache=False,
+    cache=True,
     cache_version=cache_version,
     retries=3,
     interruptible=True,
@@ -52,11 +49,11 @@ def download_data(download_dataset_args: DownloadDatasetInterface) -> FlyteFile:
 
 
 @task(
-    cache=False,
+    cache=True,
     cache_version=cache_version,
     retries=3,
     interruptible=False,
-    timeout=timedelta(minutes=20),
+    timeout=timedelta(minutes=60),
     requests=Resources(cpu="8", mem="30Gi", ephemeral_storage="16Gi"),
 )
 def preprocess_data(
@@ -79,7 +76,7 @@ def preprocess_data(
     cache_version=cache_version,
     retries=3,
     interruptible=False,
-    timeout=timedelta(minutes=60),
+    timeout=timedelta(minutes=120),
     container_image="{{.image.gpu.fqn}}:{{.image.gpu.version}}",
     requests=Resources(cpu="8", mem="30Gi", ephemeral_storage="16Gi", gpu="1"),
     accelerator=T4,
@@ -142,11 +139,15 @@ def module_workflow(
     return model_outputs
 
 
+UNIFORM_MAX_EPOCHS = 2000
+
 simulated_dataset_args = DownloadDatasetInterface(
     data_set_name="simulated",
     source="simulate",
-    n_obs=60,
-    n_vars=100,
+    n_obs=3000,
+    n_vars=2000,
+    # n_obs=200,
+    # n_vars=100,
 )
 simulated_preprocess_data_args = PreprocessDataInterface(
     data_set_name=f"{simulated_dataset_args.data_set_name}",
@@ -156,17 +157,18 @@ simulated_train_model1_args = PyroVelocityTrainInterface(
     guide_type="auto_t0_constraint",
     offset=False,
     cell_state="leiden",
-    max_epochs=800,
+    max_epochs=UNIFORM_MAX_EPOCHS,
 )
 simulated_train_model2_args = PyroVelocityTrainInterface(
     cell_state="leiden",
-    max_epochs=800,
+    max_epochs=UNIFORM_MAX_EPOCHS,
 )
+
 
 pancreas_dataset_args = DownloadDatasetInterface(
     data_set_name="pancreas",
-    n_obs=200,
-    n_vars=500,
+    # n_obs=200,
+    # n_vars=500,
 )
 pancreas_preprocess_data_args = PreprocessDataInterface(
     data_set_name=f"{pancreas_dataset_args.data_set_name}",
@@ -176,10 +178,79 @@ pancreas_preprocess_data_args = PreprocessDataInterface(
 pancreas_train_model1_args = PyroVelocityTrainInterface(
     guide_type="auto_t0_constraint",
     offset=False,
-    max_epochs=800,
+    max_epochs=UNIFORM_MAX_EPOCHS,
 )
 pancreas_train_model2_args = PyroVelocityTrainInterface(
-    max_epochs=800,
+    max_epochs=UNIFORM_MAX_EPOCHS,
+)
+
+
+pbmc68k_dataset_args = DownloadDatasetInterface(
+    data_set_name="pbmc68k",
+    # n_obs=500,
+)
+pbmc68k_preprocess_data_args = PreprocessDataInterface(
+    data_set_name=f"{pbmc68k_dataset_args.data_set_name}",
+    adata=f"{pbmc68k_dataset_args.data_external_path}/{pbmc68k_dataset_args.data_set_name}.h5ad",
+    default_velocity_mode="stochastic",
+    vector_field_basis="tsne",
+)
+pbmc68k_train_model1_args = PyroVelocityTrainInterface(
+    guide_type="auto_t0_constraint",
+    offset=False,
+    cell_state="celltype",
+    max_epochs=UNIFORM_MAX_EPOCHS,
+)
+pbmc68k_train_model2_args = PyroVelocityTrainInterface(
+    cell_state="celltype",
+    max_epochs=UNIFORM_MAX_EPOCHS,
+)
+
+
+pons_dataset_args = DownloadDatasetInterface(
+    data_set_name="pons",
+    # n_obs=200,
+    # n_vars=500,
+)
+pons_preprocess_data_args = PreprocessDataInterface(
+    data_set_name=f"{pons_dataset_args.data_set_name}",
+    adata=f"{pons_dataset_args.data_external_path}/{pons_dataset_args.data_set_name}.h5ad",
+)
+pons_train_model1_args = PyroVelocityTrainInterface(
+    guide_type="auto_t0_constraint",
+    offset=False,
+    cell_state="celltype",
+    max_epochs=UNIFORM_MAX_EPOCHS,
+)
+pons_train_model2_args = PyroVelocityTrainInterface(
+    cell_state="celltype",
+    max_epochs=UNIFORM_MAX_EPOCHS,
+)
+
+
+larry_dataset_args = DownloadDatasetInterface(
+    data_set_name="larry",
+    # n_obs=500,
+    # n_vars=2000,
+)
+larry_preprocess_data_args = PreprocessDataInterface(
+    data_set_name=f"{larry_dataset_args.data_set_name}",
+    adata=f"{larry_dataset_args.data_external_path}/{larry_dataset_args.data_set_name}.h5ad",
+    vector_field_basis="emb",
+)
+larry_train_model1_args = PyroVelocityTrainInterface(
+    guide_type="auto_t0_constraint",
+    svi_train=True,
+    batch_size=4000,
+    offset=False,
+    cell_state="state_info",
+    max_epochs=UNIFORM_MAX_EPOCHS,
+)
+larry_train_model2_args = PyroVelocityTrainInterface(
+    svi_train=True,
+    batch_size=4000,
+    cell_state="state_info",
+    max_epochs=UNIFORM_MAX_EPOCHS,
 )
 
 
@@ -193,6 +264,18 @@ def training_workflow(
     pancreas_preprocess_data_args: PreprocessDataInterface = pancreas_preprocess_data_args,
     pancreas_train_model1_args: PyroVelocityTrainInterface = pancreas_train_model1_args,
     pancreas_train_model2_args: PyroVelocityTrainInterface = pancreas_train_model2_args,
+    pbmc68k_dataset_args: DownloadDatasetInterface = pbmc68k_dataset_args,
+    pbmc68k_preprocess_data_args: PreprocessDataInterface = pbmc68k_preprocess_data_args,
+    pbmc68k_train_model1_args: PyroVelocityTrainInterface = pbmc68k_train_model1_args,
+    pbmc68k_train_model2_args: PyroVelocityTrainInterface = pbmc68k_train_model2_args,
+    pons_dataset_args: DownloadDatasetInterface = pons_dataset_args,
+    pons_preprocess_data_args: PreprocessDataInterface = pons_preprocess_data_args,
+    pons_train_model1_args: PyroVelocityTrainInterface = pons_train_model1_args,
+    pons_train_model2_args: PyroVelocityTrainInterface = pons_train_model2_args,
+    larry_dataset_args: DownloadDatasetInterface = larry_dataset_args,
+    larry_preprocess_data_args: PreprocessDataInterface = larry_preprocess_data_args,
+    larry_train_model1_args: PyroVelocityTrainInterface = larry_train_model1_args,
+    larry_train_model2_args: PyroVelocityTrainInterface = larry_train_model2_args,
 ) -> list[TrainingOutputs]:
     """
     Apply the module_workflow to all datasets.
@@ -229,32 +312,65 @@ def training_workflow(
         train_model_args=pancreas_train_model2_args,
     )
 
-    # pancreas_data = module_workflow(
-    #     download_dataset_args=DownloadDatasetInterface(
-    #         data_set_name="pancreas",
-    #     ),
-    #     train_dataset_name="pancreas",
-    # )
+    pbmc68k_model1 = module_workflow(
+        download_dataset_args=pbmc68k_dataset_args,
+        preprocess_data_args=pbmc68k_preprocess_data_args,
+        train_data_set_name="pbmc68k",
+        model_identifier="model1",
+        train_model_args=pbmc68k_train_model1_args,
+    )
 
-    # pons_data = module_workflow(
-    #     download_dataset_args=DownloadDatasetInterface(
-    #         data_set_name="pons",
-    #     ),
-    #     train_dataset_name="pons",
-    # )
+    pbmc68k_model2 = module_workflow(
+        download_dataset_args=pbmc68k_dataset_args,
+        preprocess_data_args=pbmc68k_preprocess_data_args,
+        train_data_set_name="pbmc68k",
+        model_identifier="model2",
+        train_model_args=pbmc68k_train_model2_args,
+    )
 
-    # pbmc68k_data = module_workflow(
-    #     download_dataset_args=DownloadDatasetInterface(
-    #         data_set_name="pbmc68k",
-    #     ),
-    #     train_dataset_name="pbmc68k",
-    # )
+    pons_model1 = module_workflow(
+        download_dataset_args=pons_dataset_args,
+        preprocess_data_args=pons_preprocess_data_args,
+        train_data_set_name="pons",
+        model_identifier="model1",
+        train_model_args=pons_train_model1_args,
+    )
+
+    pons_model2 = module_workflow(
+        download_dataset_args=pons_dataset_args,
+        preprocess_data_args=pons_preprocess_data_args,
+        train_data_set_name="pons",
+        model_identifier="model2",
+        train_model_args=pons_train_model2_args,
+    )
+
+    larry_model1 = module_workflow(
+        download_dataset_args=larry_dataset_args,
+        preprocess_data_args=larry_preprocess_data_args,
+        train_data_set_name="larry",
+        model_identifier="model1",
+        train_model_args=larry_train_model1_args,
+    )
+
+    larry_model2 = module_workflow(
+        download_dataset_args=larry_dataset_args,
+        preprocess_data_args=larry_preprocess_data_args,
+        train_data_set_name="larry",
+        model_identifier="model2",
+        train_model_args=larry_train_model2_args,
+    )
 
     return [
         simulated_model1,
         simulated_model2,
         pancreas_model1,
         pancreas_model2,
+        pbmc68k_model1,
+        pbmc68k_model2,
+        pons_model1,
+        pons_model2,
+        larry_model1,
+        larry_model2,
     ]
 
 
