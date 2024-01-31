799a0140c7640c56dd9860fc1b3710e1e0fc87ae
Cameron Smith <cameron.ray.smith@gmail.com>
2024-01-31 01:28:22 -0500

    refactor(main_workflow): move configuration to separate module Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/workflows/main_workflow.py
+++ src/pyrovelocity/workflows/main_workflow.py
@@ -1,11 +1,14 @@
-from dataclasses import asdict, dataclass
+import functools
+from dataclasses import asdict
 from datetime import timedelta
+from typing import Dict
 
-from flytekit import Resources, task, workflow
+# from flytekit import map_task
+from flytekit import Resources, dynamic, task, workflow
+from flytekit.experimental import map_task
 from flytekit.extras.accelerators import T4
 from flytekit.types.directory import FlyteDirectory
 from flytekit.types.file import FlyteFile
-from mashumaro.mixins.json import DataClassJSONMixin
 
 from pyrovelocity.data import download_dataset
 from pyrovelocity.interfaces import (
@@ -15,21 +18,21 @@ from pyrovelocity.interfaces import (
 )
 from pyrovelocity.logging import configure_logging
 from pyrovelocity.preprocess import preprocess_dataset
-from pyrovelocity.train import PyroVelocityTrainInterface, train_dataset
+from pyrovelocity.train import train_dataset
+from pyrovelocity.workflows.main_configuration import (
+    ResourcesJSON,
+    TrainingOutputs,
+    default_training_resources,
+    larry_configuration,
+    pancreas_configuration,
+    pbmc68k_configuration,
+    pons_configuration,
+    simulated_configuration,
+)
 
 logger = configure_logging(__name__)
 
 cache_version = "0.2.0b8"
-
-
-@dataclass
-class TrainingOutputs(DataClassJSONMixin):
-    trained_data_path: FlyteFile
-    model_path: FlyteDirectory
-    posterior_samples_path: FlyteFile
-    metrics_path: FlyteFile
-    run_info_path: FlyteFile
-    loss_plot_path: FlyteFile
 
 
 @task(
@@ -80,24 +83,20 @@ def preprocess_data(
     interruptible=False,
     timeout=timedelta(minutes=120),
     container_image="{{.image.gpu.fqn}}:{{.image.gpu.version}}",
-    requests=Resources(
-        cpu="16", mem="120Gi", ephemeral_storage="32Gi", gpu="1"
-    ),
-    limits=Resources(cpu="32", mem="200Gi", ephemeral_storage="150Gi", gpu="1"),
+    requests=Resources(cpu="8", mem="30Gi", ephemeral_storage="50Gi", gpu="1"),
+    limits=Resources(cpu="16", mem="60Gi", ephemeral_storage="200Gi", gpu="1"),
     accelerator=T4,
 )
 def train_model(
     data: FlyteFile,
-    data_set_name: str,
-    model_identifier: str,
-    train_model_args: PyroVelocityTrainInterface,
+    train_model_configuration: PyroVelocityTrainInterface,
 ) -> TrainingOutputs:
     """
     Train model.
     """
     data_path = data.download()
     print(f"Flyte train model input data path: {data_path}")
-    train_model_args.adata = str(data_path)
+    train_model_configuration.adata = str(data_path)
     (
         trained_data_path,
         model_path,
@@ -106,9 +105,7 @@ def train_model(
         run_info_path,
         loss_plot_path,
     ) = train_dataset(
-        data_set_name=data_set_name,
-        model_identifier=model_identifier,
-        pyrovelocity_train_model_args=train_model_args,
+        **asdict(train_model_configuration),
     )
     return TrainingOutputs(
         trained_data_path=FlyteFile(path=trained_data_path),
@@ -120,262 +117,164 @@ def train_model(
     )
 
 
-@workflow
+@dynamic
 def module_workflow(
     download_dataset_args: DownloadDatasetInterface = DownloadDatasetInterface(),
     preprocess_data_args: PreprocessDataInterface = PreprocessDataInterface(),
-    train_data_set_name: str = "simulated",
-    model_identifier: str = "model2",
-    train_model_args: PyroVelocityTrainInterface = PyroVelocityTrainInterface(),
-) -> TrainingOutputs:
+    train_model_configurations: list[PyroVelocityTrainInterface] = [
+        PyroVelocityTrainInterface()
+    ],
+    train_model_resources: list[ResourcesJSON] = default_training_resources,
+) -> list[TrainingOutputs]:
     """
-    Put all of the steps together into a single workflow.
+    The module workflow is applied to a single dataset together with a list of
+    models.
+
+    There are three ways to execute the train_model() task, which impacts how
+    subsequent tasks are executed. The first two use the @workflow decorator
+    and the last requires the @dynamic decorator.
+
+
+    1. For independent parallel execution treating each data-model pairing as
+       requiring a separate workflow run, ensure the decorator is @workflow.
+       Note that individual tasks are easily re-run in this case. For this
+       reason, this is the preferred method during development, but since
+       @dynamic is required in order to dynamically determine resource
+       requirements, method three may be preferred in production. A single
+       model configuration is required in this case and a single set of training
+       outputs is produced.
+
+    ```python
+    train_model_configuration: PyroVelocityTrainInterface = PyroVelocityTrainInterface(),
+    ) -> TrainingOutputs:
+    model_outputs = train_model(
+        data=processed_data,
+        train_model_configuration=train_model_configuration,
+    )
+    ```
+
+    2. For map task-based execution, ensure the decorator is @workflow.
+       Note that individual tasks are not easily re-run in this case.
+
+    ```python
+    partial_train_model = functools.partial(train_model, data=processed_data)
+    model_outputs = map_task(partial_train_model)(
+        train_model_args=train_model_configurations,
+    )
+    ```
+
+    3. For dynamic workflow-based execution, ensure the decorator is @dynamic.
+       Note that individual tasks are not easily re-run in this case.
     """
     data = download_data(download_dataset_args=download_dataset_args)
     processed_data = preprocess_data(
         data=data, preprocess_data_args=preprocess_data_args
     )
-    model_outputs = train_model(
-        data=processed_data,
-        data_set_name=train_data_set_name,
-        model_identifier=model_identifier,
-        train_model_args=train_model_args,
-    )
+
+    model_outputs: list[TrainingOutputs] = list()
+    for train_model_configuration in train_model_configurations:
+        model_output = train_model(
+            data=processed_data,
+            train_model_configuration=train_model_configuration,
+        ).with_overrides(
+            requests=Resources(**asdict(train_model_resources[0])),
+            limits=Resources(**asdict(train_model_resources[1])),
+        )
+        model_outputs.append(model_output)
     return model_outputs
-
-
-UNIFORM_MAX_EPOCHS = 2000
-
-simulated_dataset_args = DownloadDatasetInterface(
-    data_set_name="simulated",
-    source="simulate",
-    n_obs=3000,
-    n_vars=2000,
-    # n_obs=200,
-    # n_vars=100,
-)
-simulated_preprocess_data_args = PreprocessDataInterface(
-    data_set_name=f"{simulated_dataset_args.data_set_name}",
-    adata=f"{simulated_dataset_args.data_external_path}/{simulated_dataset_args.data_set_name}.h5ad",
-)
-simulated_train_model1_args = PyroVelocityTrainInterface(
-    guide_type="auto_t0_constraint",
-    offset=False,
-    cell_state="leiden",
-    max_epochs=UNIFORM_MAX_EPOCHS,
-)
-simulated_train_model2_args = PyroVelocityTrainInterface(
-    cell_state="leiden",
-    max_epochs=UNIFORM_MAX_EPOCHS,
-)
-
-
-pancreas_dataset_args = DownloadDatasetInterface(
-    data_set_name="pancreas",
-    # n_obs=200,
-    # n_vars=500,
-)
-pancreas_preprocess_data_args = PreprocessDataInterface(
-    data_set_name=f"{pancreas_dataset_args.data_set_name}",
-    adata=f"{pancreas_dataset_args.data_external_path}/{pancreas_dataset_args.data_set_name}.h5ad",
-    process_cytotrace=True,
-)
-pancreas_train_model1_args = PyroVelocityTrainInterface(
-    guide_type="auto_t0_constraint",
-    offset=False,
-    max_epochs=UNIFORM_MAX_EPOCHS,
-)
-pancreas_train_model2_args = PyroVelocityTrainInterface(
-    max_epochs=UNIFORM_MAX_EPOCHS,
-)
-
-
-pbmc68k_dataset_args = DownloadDatasetInterface(
-    data_set_name="pbmc68k",
-    # n_obs=500,
-)
-pbmc68k_preprocess_data_args = PreprocessDataInterface(
-    data_set_name=f"{pbmc68k_dataset_args.data_set_name}",
-    adata=f"{pbmc68k_dataset_args.data_external_path}/{pbmc68k_dataset_args.data_set_name}.h5ad",
-    default_velocity_mode="stochastic",
-    vector_field_basis="tsne",
-)
-pbmc68k_train_model1_args = PyroVelocityTrainInterface(
-    guide_type="auto_t0_constraint",
-    offset=False,
-    cell_state="celltype",
-    max_epochs=UNIFORM_MAX_EPOCHS,
-)
-pbmc68k_train_model2_args = PyroVelocityTrainInterface(
-    cell_state="celltype",
-    max_epochs=UNIFORM_MAX_EPOCHS,
-)
-
-
-pons_dataset_args = DownloadDatasetInterface(
-    data_set_name="pons",
-    # n_obs=200,
-    # n_vars=500,
-)
-pons_preprocess_data_args = PreprocessDataInterface(
-    data_set_name=f"{pons_dataset_args.data_set_name}",
-    adata=f"{pons_dataset_args.data_external_path}/{pons_dataset_args.data_set_name}.h5ad",
-)
-pons_train_model1_args = PyroVelocityTrainInterface(
-    guide_type="auto_t0_constraint",
-    offset=False,
-    cell_state="celltype",
-    max_epochs=UNIFORM_MAX_EPOCHS,
-)
-pons_train_model2_args = PyroVelocityTrainInterface(
-    cell_state="celltype",
-    max_epochs=UNIFORM_MAX_EPOCHS,
-)
-
-
-larry_dataset_args = DownloadDatasetInterface(
-    data_set_name="larry",
-    # n_obs=500,
-    # n_vars=2000,
-)
-larry_preprocess_data_args = PreprocessDataInterface(
-    data_set_name=f"{larry_dataset_args.data_set_name}",
-    adata=f"{larry_dataset_args.data_external_path}/{larry_dataset_args.data_set_name}.h5ad",
-    vector_field_basis="emb",
-)
-larry_train_model1_args = PyroVelocityTrainInterface(
-    guide_type="auto_t0_constraint",
-    svi_train=True,
-    batch_size=4000,
-    offset=False,
-    cell_state="state_info",
-    max_epochs=UNIFORM_MAX_EPOCHS,
-)
-larry_train_model2_args = PyroVelocityTrainInterface(
-    svi_train=True,
-    batch_size=4000,
-    cell_state="state_info",
-    max_epochs=UNIFORM_MAX_EPOCHS,
-)
 
 
 @workflow
 def training_workflow(
-    simulated_dataset_args: DownloadDatasetInterface = simulated_dataset_args,
-    simulated_preprocess_data_args: PreprocessDataInterface = simulated_preprocess_data_args,
-    simulated_train_model1_args: PyroVelocityTrainInterface = simulated_train_model1_args,
-    simulated_train_model2_args: PyroVelocityTrainInterface = simulated_train_model2_args,
-    pancreas_dataset_args: DownloadDatasetInterface = pancreas_dataset_args,
-    pancreas_preprocess_data_args: PreprocessDataInterface = pancreas_preprocess_data_args,
-    pancreas_train_model1_args: PyroVelocityTrainInterface = pancreas_train_model1_args,
-    pancreas_train_model2_args: PyroVelocityTrainInterface = pancreas_train_model2_args,
-    pbmc68k_dataset_args: DownloadDatasetInterface = pbmc68k_dataset_args,
-    pbmc68k_preprocess_data_args: PreprocessDataInterface = pbmc68k_preprocess_data_args,
-    pbmc68k_train_model1_args: PyroVelocityTrainInterface = pbmc68k_train_model1_args,
-    pbmc68k_train_model2_args: PyroVelocityTrainInterface = pbmc68k_train_model2_args,
-    pons_dataset_args: DownloadDatasetInterface = pons_dataset_args,
-    pons_preprocess_data_args: PreprocessDataInterface = pons_preprocess_data_args,
-    pons_train_model1_args: PyroVelocityTrainInterface = pons_train_model1_args,
-    pons_train_model2_args: PyroVelocityTrainInterface = pons_train_model2_args,
-    larry_dataset_args: DownloadDatasetInterface = larry_dataset_args,
-    larry_preprocess_data_args: PreprocessDataInterface = larry_preprocess_data_args,
-    larry_train_model1_args: PyroVelocityTrainInterface = larry_train_model1_args,
-    larry_train_model2_args: PyroVelocityTrainInterface = larry_train_model2_args,
-) -> list[TrainingOutputs]:
+    simulated_dataset_configuration: DownloadDatasetInterface = simulated_configuration.download_dataset,
+    simulated_preprocess_configuration: PreprocessDataInterface = simulated_configuration.preprocess_data,
+    simulated_train_model_configurations: list[
+        PyroVelocityTrainInterface
+    ] = simulated_configuration.training_configurations,
+    simulated_train_model_resources: list[
+        ResourcesJSON
+    ] = simulated_configuration.training_resources,
+    pancreas_dataset_configuration: DownloadDatasetInterface = pancreas_configuration.download_dataset,
+    pancreas_preprocess_configuration: PreprocessDataInterface = pancreas_configuration.preprocess_data,
+    pancreas_train_model_configurations: list[
+        PyroVelocityTrainInterface
+    ] = pancreas_configuration.training_configurations,
+    pancreas_train_model_resources: list[
+        ResourcesJSON
+    ] = pancreas_configuration.training_resources,
+    pbmc68k_dataset_configuration: DownloadDatasetInterface = pbmc68k_configuration.download_dataset,
+    pbmc68k_preprocess_configuration: PreprocessDataInterface = pbmc68k_configuration.preprocess_data,
+    pbmc68k_train_model_configurations: list[
+        PyroVelocityTrainInterface
+    ] = pbmc68k_configuration.training_configurations,
+    pbmc68k_train_model_resources: list[
+        ResourcesJSON
+    ] = pbmc68k_configuration.training_resources,
+    pons_dataset_configuration: DownloadDatasetInterface = pons_configuration.download_dataset,
+    pons_preprocess_configuration: PreprocessDataInterface = pons_configuration.preprocess_data,
+    pons_train_model_configurations: list[
+        PyroVelocityTrainInterface
+    ] = pons_configuration.training_configurations,
+    pons_train_model_resources: list[
+        ResourcesJSON
+    ] = pons_configuration.training_resources,
+    larry_dataset_configuration: DownloadDatasetInterface = larry_configuration.download_dataset,
+    larry_preprocess_configuration: PreprocessDataInterface = larry_configuration.preprocess_data,
+    larry_train_model_configurations: list[
+        PyroVelocityTrainInterface
+    ] = larry_configuration.training_configurations,
+    larry_train_model_resources: list[
+        ResourcesJSON
+    ] = larry_configuration.training_resources,
+) -> list[list[TrainingOutputs]]:
     """
     Apply the module_workflow to all datasets.
+
+    TODO: update interface extraction to support nested dataclasses to simplify
+    inputs to:
+    simulated_configuration: WorkflowConfiguration = simulated_configuration,
     """
-    simulated_model1 = module_workflow(
-        download_dataset_args=simulated_dataset_args,
-        preprocess_data_args=simulated_preprocess_data_args,
-        train_data_set_name="simulated",
-        model_identifier="model1",
-        train_model_args=simulated_train_model1_args,
+    simulated = module_workflow(
+        download_dataset_args=simulated_dataset_configuration,
+        preprocess_data_args=simulated_preprocess_configuration,
+        train_model_configurations=simulated_train_model_configurations,
+        train_model_resources=simulated_train_model_resources,
     )
 
-    simulated_model2 = module_workflow(
-        download_dataset_args=simulated_dataset_args,
-        preprocess_data_args=simulated_preprocess_data_args,
-        train_data_set_name="simulated",
-        model_identifier="model2",
-        train_model_args=simulated_train_model2_args,
+    pancreas = module_workflow(
+        download_dataset_args=pancreas_dataset_configuration,
+        preprocess_data_args=pancreas_preprocess_configuration,
+        train_model_configurations=pancreas_train_model_configurations,
+        train_model_resources=pancreas_train_model_resources,
     )
 
-    pancreas_model1 = module_workflow(
-        download_dataset_args=pancreas_dataset_args,
-        preprocess_data_args=pancreas_preprocess_data_args,
-        train_data_set_name="pancreas",
-        model_identifier="model1",
-        train_model_args=pancreas_train_model1_args,
+    pbmc68k = module_workflow(
+        download_dataset_args=pbmc68k_dataset_configuration,
+        preprocess_data_args=pbmc68k_preprocess_configuration,
+        train_model_configurations=pbmc68k_train_model_configurations,
+        train_model_resources=pbmc68k_train_model_resources,
     )
 
-    pancreas_model2 = module_workflow(
-        download_dataset_args=pancreas_dataset_args,
-        preprocess_data_args=pancreas_preprocess_data_args,
-        train_data_set_name="pancreas",
-        model_identifier="model2",
-        train_model_args=pancreas_train_model2_args,
+    pons = module_workflow(
+        download_dataset_args=pons_dataset_configuration,
+        preprocess_data_args=pons_preprocess_configuration,
+        train_model_configurations=pons_train_model_configurations,
+        train_model_resources=pons_train_model_resources,
     )
 
-    pbmc68k_model1 = module_workflow(
-        download_dataset_args=pbmc68k_dataset_args,
-        preprocess_data_args=pbmc68k_preprocess_data_args,
-        train_data_set_name="pbmc68k",
-        model_identifier="model1",
-        train_model_args=pbmc68k_train_model1_args,
-    )
-
-    pbmc68k_model2 = module_workflow(
-        download_dataset_args=pbmc68k_dataset_args,
-        preprocess_data_args=pbmc68k_preprocess_data_args,
-        train_data_set_name="pbmc68k",
-        model_identifier="model2",
-        train_model_args=pbmc68k_train_model2_args,
-    )
-
-    pons_model1 = module_workflow(
-        download_dataset_args=pons_dataset_args,
-        preprocess_data_args=pons_preprocess_data_args,
-        train_data_set_name="pons",
-        model_identifier="model1",
-        train_model_args=pons_train_model1_args,
-    )
-
-    pons_model2 = module_workflow(
-        download_dataset_args=pons_dataset_args,
-        preprocess_data_args=pons_preprocess_data_args,
-        train_data_set_name="pons",
-        model_identifier="model2",
-        train_model_args=pons_train_model2_args,
-    )
-
-    larry_model1 = module_workflow(
-        download_dataset_args=larry_dataset_args,
-        preprocess_data_args=larry_preprocess_data_args,
-        train_data_set_name="larry",
-        model_identifier="model1",
-        train_model_args=larry_train_model1_args,
-    )
-
-    larry_model2 = module_workflow(
-        download_dataset_args=larry_dataset_args,
-        preprocess_data_args=larry_preprocess_data_args,
-        train_data_set_name="larry",
-        model_identifier="model2",
-        train_model_args=larry_train_model2_args,
+    larry = module_workflow(
+        download_dataset_args=larry_dataset_configuration,
+        preprocess_data_args=larry_preprocess_configuration,
+        train_model_configurations=larry_train_model_configurations,
+        train_model_resources=larry_train_model_resources,
     )
 
     return [
-        simulated_model1,
-        simulated_model2,
-        pancreas_model1,
-        pancreas_model2,
-        pbmc68k_model1,
-        pbmc68k_model2,
-        pons_model1,
-        pons_model2,
-        larry_model1,
-        larry_model2,
+        simulated,
+        pancreas,
+        pbmc68k,
+        pons,
+        larry,
     ]
 
 
