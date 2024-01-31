409d96dc5c8a63a036cb4b2892b5793d23280e4f
Cameron Smith <cameron.ray.smith@gmail.com>
2024-01-31 15:38:55 -0500

    feat(conftest): provide global fixture to file for sample data Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/tests/conftest.py
+++ src/pyrovelocity/tests/conftest.py
@@ -11,3 +11,11 @@ def default_sample_data():
         AnnData: AnnData object to use for testing.
     """
     return generate_sample_data(random_seed=98)
+
+
+@pytest.fixture
+def default_sample_data_file(default_sample_data, tmp_path):
+    """Create a .h5ad file from the default sample data."""
+    file_path = tmp_path / "default_sample_data.h5ad"
+    default_sample_data.write(file_path)
+    return file_path
