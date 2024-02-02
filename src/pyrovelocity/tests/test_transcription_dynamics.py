c9c5416b2bd99fb557dbb3c5421cdb000c71624f
Cameron Smith <cameron.ray.smith@gmail.com>
2024-02-02 12:53:39 -0500

    test(transcription_dynamics): add test module Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

---
+++ src/pyrovelocity/tests/test_transcription_dynamics.py
@@ -0,0 +1,7 @@
+"""Tests for `pyrovelocity._transcription_dynamics` module."""
+
+
+def test_load__transcription_dynamics():
+    from pyrovelocity import _transcription_dynamics
+
+    print(_transcription_dynamics.__file__)
