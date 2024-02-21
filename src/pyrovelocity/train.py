6af93eaee91fc49640259050e6daff8b9d2ccbad
Cameron Smith <cameron.ray.smith@gmail.com>
2024-02-20 19:58:24 -0500

    fix(train): use tmp_path fixture for loss plot Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/train.py
+++ src/pyrovelocity/train.py
@@ -310,9 +310,12 @@ def train_model(
         >>> from pyrovelocity.train import train_model
         >>> from pyrovelocity.utils import generate_sample_data
         >>> from pyrovelocity.preprocess import copy_raw_counts
+        >>> tmp = getfixture("tmp_path")
+        >>> loss_plot_path = str(tmp) + "/loss_plot_docs.png"
+        >>> print(loss_plot_path)
         >>> adata = generate_sample_data(random_seed=99)
         >>> copy_raw_counts(adata)
-        >>> _, model, posterior_samples = train_model(adata, use_gpu=False, seed=99, max_epochs=200, loss_plot_path="loss_plot_docs.png")
+        >>> _, model, posterior_samples = train_model(adata, use_gpu=False, seed=99, max_epochs=200, loss_plot_path=loss_plot_path)
     """
     if isinstance(adata, str):
         adata = load_anndata_from_path(adata)
