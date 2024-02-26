3b68fff956b8f8e1ac5b10d8611613dd7474deb6
Cameron Smith <cameron.ray.smith@gmail.com>
2024-02-25 20:04:29 -0500

    fix(trainer): migrate epoch-end hooks for lightning >2
    https://www.github.com/Lightning-AI/pytorch-lightning/pull/16520
    
    - validation set cannot be empty
      - https://www.github.com/Lightning-AI/pytorch-lightning/pull/16800#discussion_r1110256813
    - `use_gpu` with lightning AcceleratorRegistry keywords
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/models/_trainer.py
+++ src/pyrovelocity/models/_trainer.py
@@ -1,27 +1,23 @@
 import math
-from typing import Any
-from typing import Callable
-from typing import Dict
-from typing import List
-from typing import Optional
-from typing import Sequence
-from typing import Union
+from typing import Any, Callable, Dict, List, Optional, Sequence, Union
 
 import mlflow
 import numpy as np
 import pyro
 import scipy
 import torch
-from pyro.infer import Trace_ELBO
-from pyro.infer import TraceEnum_ELBO
+from pyro.infer import Trace_ELBO, TraceEnum_ELBO
 from pyro.infer.autoguide.guides import AutoGuideList
 from pyro.optim.clipped_adam import ClippedAdam
 from pyro.optim.optim import PyroOptim
 from scvi.dataloaders import DataSplitter
-from scvi.train import PyroTrainingPlan
-from scvi.train import TrainRunner
+from scvi.model._utils import parse_device_args
+from scvi.train import PyroTrainingPlan, TrainRunner
 
+from pyrovelocity.logging import configure_logging
 from pyrovelocity.models._velocity_module import VelocityModule
+
+logger = configure_logging(__name__)
 
 
 class VelocityAdam(ClippedAdam):
@@ -42,12 +38,12 @@ class VelocityAdam(ClippedAdam):
                 grad[grad.isnan()] = 0.0
                 state = self.state[p]
 
-                # State initialization
+                # initialize state
                 if len(state) == 0:
                     state["step"] = 0
-                    # Exponential moving average of gradient values
+                    # compute exponential moving average of gradient values
                     state["exp_avg"] = torch.zeros_like(grad)
-                    # Exponential moving average of squared gradient values
+                    # compute exponential moving average of squared gradient values
                     state["exp_avg_sq"] = torch.zeros_like(grad)
 
                 exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
@@ -58,7 +54,6 @@ class VelocityAdam(ClippedAdam):
                 if group["weight_decay"] != 0:
                     grad = grad.add(p.data, alpha=group["weight_decay"])
 
-                # Decay the first and second moment running average coefficient
                 exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
                 exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
 
@@ -99,59 +94,64 @@ class EnumTrainingPlan(PyroTrainingPlan):
             loss=self.loss_fn,
         )
         self.n_elem = self.module.num_genes * self.module.num_cells * 2
+        self.training_step_outputs = []
+        self.validation_step_outputs = []
 
-    def training_step(self, batch, batch_idx, optimizer_idx=0):
+    # previously required optimizer_idx argument is no longer used
+    def training_step(self, batch, batch_idx):
         args, kwargs = self.module._get_fn_args_from_batch(batch)
         loss = self.svi.step(*args, **kwargs)
+
+        self.training_step_outputs.append(
+            {
+                "train_step_loss": loss,
+                "num_elem": args[0].shape[0] * args[0].shape[1] * 2,
+            }
+        )
         return {
             "train_step_loss": loss,
             "num_elem": args[0].shape[0] * args[0].shape[1] * 2,
         }
 
-    def training_epoch_end(self, outputs):
+    def on_train_epoch_end(self):
         n_batch, elbo = 0, 0
-        n_elem = 0
-
-        for tensors in outputs:
+        for tensors in self.training_step_outputs:
             elbo += tensors["train_step_loss"]
             n_batch += 1
-            # n_elem += tensors['num_elem']
-        self.log("elbo_train", elbo / n_batch, prog_bar=True, on_epoch=True)
-        # self.log("elbo_train", elbo / self.n_elem, prog_bar=True, on_epoch=True)
-        # self.log("elbo_train", elbo / n_elem, prog_bar=True, on_epoch=True)
+        if n_batch > 0:
+            self.log("elbo_train", elbo / n_batch, prog_bar=True, on_epoch=True)
+
+        self.training_step_outputs.clear()
 
     def validation_step(self, batch, batch_idx):
-        args, kwargs = self.pyro_velocity._get_fn_args_from_batch(batch)
+        args, kwargs = self.module._get_fn_args_from_batch(batch)
         loss = self.svi.evaluate_loss(*args, **kwargs)
         return {
             "valid_step_loss": loss,
             "num_elem": args[0].shape[0] * args[0].shape[1] * 2,
         }
 
-    def validation_epoch_end(self, outputs):
-        """Aggregate validation step information."""
+    def on_validation_epoch_end(self):
         n_batch, elbo = 0, 0
-        n_elem = 0
-        for tensors in outputs:
+        for tensors in self.validation_step_outputs:
             elbo += tensors["valid_step_loss"]
             n_batch += 1
-            ##n_elem += tensors['num_elem']
-        self.log(
-            "elbo_validation", elbo / n_batch, prog_bar=True, on_epoch=True
-        )
-        # self.log("elbo_validation", elbo / self.n_elem, prog_bar=True, on_epoch=True)
-        ##self.log("elbo_validation", elbo / n_elem, prog_bar=True, on_epoch=True)
+        if n_batch > 0:
+            self.log(
+                "elbo_validation", elbo / n_batch, prog_bar=True, on_epoch=True
+            )
+        self.validation_step_outputs.clear()
 
 
 class VelocityTrainingMixin:
     def train(
         self,
-        use_gpu: Optional[Union[str, int, bool]] = 0,
+        use_gpu: str = "auto",
         early_stopping: bool = False,
         seed: int = 99,
         lr: float = 1e-3,
-        train_size: float = 1.0,
-        valid_size: float = 0.0,
+        train_size: float = 0.995,
+        valid_size: float = 0.005,
         batch_size: int = 256,
         max_epochs: int = 100,
         check_val_every_n_epoch: Optional[int] = 1,
@@ -159,7 +159,11 @@ class VelocityTrainingMixin:
         min_delta: float = 0.0,
         **kwargs,
     ):
-        print("base train function")
+        logger.info(
+            f"\ntrain model:\n"
+            f"\ttraining fraction: {train_size}\n"
+            f"\tvalidation fraction: {valid_size}\n"
+        )
         pyro.clear_param_store()
         pyro.set_rng_seed(seed)
 
@@ -168,8 +172,9 @@ class VelocityTrainingMixin:
             train_size=train_size,
             validation_size=valid_size,
             batch_size=batch_size,
-            use_gpu=use_gpu,
         )
+        data_splitter.setup()
+
         training_plan = EnumTrainingPlan(
             self.module, VelocityClippedAdam({"lr": lr, "lrd": 0.9999})
         )
@@ -179,7 +184,7 @@ class VelocityTrainingMixin:
             data_splitter=data_splitter,
             check_val_every_n_epoch=check_val_every_n_epoch,
             max_epochs=max_epochs,
-            use_gpu=use_gpu,
+            accelerator=use_gpu,
             early_stopping=early_stopping,
             early_stopping_patience=patience,
             early_stopping_min_delta=min_delta,
@@ -189,7 +194,7 @@ class VelocityTrainingMixin:
 
     def train_faster(
         self,
-        use_gpu: Optional[Union[str, int, bool]] = 0,
+        use_gpu: str = "auto",
         seed: int = 99,
         lr: float = 0.01,
         max_epochs: int = 5000,
@@ -197,15 +202,37 @@ class VelocityTrainingMixin:
         patient_init: int = 45,
         patient_improve: float = 0.001,
     ) -> List[float]:
-        """this method input all adata input gpu for faster IO,
-        increase larger dataset by 5-6 fold, however should not
-        be used for >20k cells with less than 40GB GPU memory,
-        this ignores validation cells"""
-        print("train_faster")
-        if (use_gpu is False) or (use_gpu == "cpu") or (use_gpu == -1):
-            device = "cpu"
-        else:
-            device = f"cuda:{use_gpu}"
+        """
+        Train model with all data loaded into accelerator memory. 20k vars by 2k
+        obs will consume approximately 40GB of GPU memory.
+
+        Args:
+            use_gpu (str, optional): _description_. Defaults to "auto".
+            seed (int, optional): _description_. Defaults to 99.
+            lr (float, optional): _description_. Defaults to 0.01.
+            max_epochs (int, optional): _description_. Defaults to 5000.
+            log_every (int, optional): _description_. Defaults to 100.
+            patient_init (int, optional): _description_. Defaults to 45.
+            patient_improve (float, optional): _description_. Defaults to 0.001.
+
+        Returns:
+            List[float]: _description_
+        """
+
+        logger.info(
+            "train model in a single batch with all data loaded into accelerator memory"
+        )
+
+        _accelerator, _devices, device = parse_device_args(
+            accelerator=use_gpu, return_device="torch"
+        )
+        logger.info(
+            f"Loading model with:\n"
+            f"\taccelerator: {_accelerator}\n"
+            f"\tdevices: {_devices}\n"
+            f"\tdevice: {device}\n"
+        )
+
         pyro.clear_param_store()
         pyro.set_rng_seed(seed)
         pyro.enable_validation(True)
@@ -216,7 +243,6 @@ class VelocityTrainingMixin:
             self.module._model,
             self.module._guide,
             optim,
-            # TraceEnum_ELBO(strict_enumeration_warning=True),
             Trace_ELBO(strict_enumeration_warning=True),
         )
 
@@ -355,7 +381,7 @@ class VelocityTrainingMixin:
 
     def train_faster_with_batch(
         self,
-        use_gpu: Optional[Union[str, int, bool]] = 0,
+        use_gpu: str = "auto",
         seed: int = 99,
         lr: float = 1e-2,
         max_epochs: int = 5000,
@@ -367,11 +393,18 @@ class VelocityTrainingMixin:
         patient_improve: float = 0.0,
         elbo_name: str = "-ELBO",
     ):
-        print("train_faster_with_batch")
-        if (use_gpu is False) or (use_gpu == "cpu") or (use_gpu == -1):
-            device = "cpu"
-        else:
-            device = f"cuda:{use_gpu}"
+        logger.info("train model in batches of size {batch_size}")
+
+        _accelerator, _devices, device = parse_device_args(
+            accelerator=use_gpu, return_device="torch"
+        )
+        logger.info(
+            f"Loading model with:\n"
+            f"\taccelerator: {_accelerator}\n"
+            f"\tdevices: {_devices}\n"
+            f"\tdevice: {device}\n"
+        )
+
         pyro.clear_param_store()
         pyro.set_rng_seed(seed)
         pyro.enable_validation(True)
@@ -400,7 +433,6 @@ class VelocityTrainingMixin:
 
         losses = []
         patience = patient_init
-        # normalizer = self.adata.shape[0] * self.adata.shape[1] * 2
         for step in range(max_epochs):
             n_batch = 0
             elbos = 0
@@ -410,7 +442,6 @@ class VelocityTrainingMixin:
                 loss = svi.step(*args, **kwargs)
                 elbos += loss
                 n_batch += 1
-            # elbos = elbos / normalizer #n_batch
             elbos = elbos / n_batch
             if (step == 0) or (
                 ((step + 1) % log_every == 0) and ((step + 1) < max_epochs)
@@ -422,7 +453,6 @@ class VelocityTrainingMixin:
             if step > log_every:
                 if (losses[-1] - elbos) < losses[-1] * patient_improve:
                     patience -= 1
-                    # print(f"step {step: >4d} loss = {elbos:0.6g} patience = {patience}")
                 else:
                     patience = patient_init
             if patience <= 0:
