4fe27ae536fd0bc61c2dd2d40eab655a3296da18
Cameron Smith <cameron.ray.smith@gmail.com>
2024-04-01 13:58:01 -0400

    feat(analysis): create module to generate transcriptome properties database Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/analysis/__init__.py
+++ src/pyrovelocity/analysis/__init__.py
@@ -1,8 +1,10 @@
 import pyrovelocity.analysis.analyze
 import pyrovelocity.analysis.cytotrace
+import pyrovelocity.analysis.transcriptome_properties
 
 
 __all__ = [
     "analyze",
     "cytotrace",
+    "transcriptome_properties",
 ]
