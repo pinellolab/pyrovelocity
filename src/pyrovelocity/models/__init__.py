4293af6ec0725e4c2ac747fbb1641db842235b24
Cameron Smith <cameron.ray.smith@gmail.com>
2024-04-06 19:08:52 -0400

    fix(models): export deterministic simulation solver Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/models/__init__.py
+++ src/pyrovelocity/models/__init__.py
@@ -1,5 +1,12 @@
+from pyrovelocity.models._deterministic_simulation import (
+    solve_transcription_splicing_model,
+)
 from pyrovelocity.models._transcription_dynamics import mrna_dynamics
 from pyrovelocity.models._velocity import PyroVelocity
 
 
-__all__ = [mrna_dynamics, PyroVelocity]
+__all__ = [
+    mrna_dynamics,
+    PyroVelocity,
+    solve_transcription_splicing_model,
+]
