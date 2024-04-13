495fde2d54be0000886fd9a4623bb8301bee3c1c
Cameron Smith <cameron.ray.smith@gmail.com>
2024-04-13 02:38:19 -0400

    fix(models): update deterministic inference Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/models/_deterministic_inference.py
+++ src/pyrovelocity/models/_deterministic_inference.py
@@ -35,17 +35,17 @@ TimeTensor = Float[
     # ------|---------------------
     # 60    | 1
     "number_of_cells \
-     number_of_timepoints_per_cell",
+     number_of_timepoints",
 ]
 MultiModalTranscriptomeTensor = Float[
     ArrayLike,
-    # Cells | Genes per cell | Timepoints per cell| Modalities per cell
-    # ------|----------------|--------------------|-------------------
-    # 60    | 1              | 1                  | 2
-    "number_of_cells \
-     number_of_genes_per_cell \
-     number_of_timepoints_per_cell \
-     number_of_modalities_per_cell",
+    # Genes | Cells | Timepoints per cell| Modalities
+    # ------|-------|--------------------|-------------------
+    # 1     | 60    | 1                  | 2
+    "number_of_genes \
+     number_of_cells \
+     number_of_timepoints \
+     number_of_modalities",
 ]
 
 
@@ -59,61 +59,67 @@ def deterministic_transcription_splicing_probabilistic_model(
     # priors
     initial_conditions = numpyro.sample(
         "initial_conditions",
-        dist.LogNormal(loc=jnp.log(1.0), scale=0.1).expand(
-            [num_genes, num_modalities]
+        dist.LogNormal(loc=jnp.log(1.0), scale=0.1),
+        sample_shape=(num_genes, num_modalities),
+    )
+    gamma = numpyro.sample(
+        "gamma",
+        dist.LogNormal(loc=jnp.log(2.0), scale=0.5),
+        sample_shape=(num_genes,),
+    )
+    sigma = numpyro.sample(
+        "sigma",
+        dist.HalfNormal(scale=1.0),
+        sample_shape=(num_modalities,),
+    )
+
+    def model_solver(gene_index, cell_index):
+        init_cond = initial_conditions[gene_index]
+        rate = gamma[gene_index]
+        t = times[cell_index, :]
+        solution = solve_transcription_splicing_model(t, init_cond, (rate,))
+        u_pred, s_pred = solution.ys[:, 0], solution.ys[:, 1]
+        return u_pred, s_pred
+
+    # mapping of numerical integration over genes and cells
+    u_pred, s_pred = jax.vmap(
+        jax.vmap(
+            model_solver,
+            in_axes=(0, None),
         ),
-    )
-    rate_params = numpyro.sample(
-        "rate_params",
-        dist.LogNormal(loc=jnp.log(2.0), scale=0.5).expand([num_genes]),
-    )
-    noise_std = numpyro.sample(
-        "noise_std", dist.HalfNormal(scale=1.0).expand([2])
+        in_axes=(None, 0),
+    )(
+        jnp.arange(num_genes),
+        jnp.arange(num_cells),
     )
 
-    for i in range(num_genes):
-        u0_i, s0_i = initial_conditions[i]
-        gamma_i = rate_params[i]
-        sigma_u, sigma_s = noise_std
-
-        for j in range(num_cells):
-            t_j = times[j]
-            assert t_j.shape == (num_timepoints,)
-
-            initial_state = jnp.array([u0_i, s0_i])
-
-            # numerically integrate the ODEs to estimate
-            # alues of u^* and s^* at each timepoint
-            solution = solve_transcription_splicing_model(
-                t_j, initial_state, jnp.array([gamma_i])
-            )
-            u_pred, s_pred = solution.ys[:, 0], solution.ys[:, 1]
-
-            # observations for
-            # [
-            #  each gene i,
-            #  and cell j,
-            #  at given time points,
-            #  for each modality,
-            # ]
-            u_obs = data[j, i, :, 0]
-            s_obs = data[j, i, :, 1]
-
-            # likelihood
-            numpyro.sample(
-                f"u_obs_{i}_{j}", dist.Normal(u_pred, sigma_u), obs=u_obs
-            )
-            numpyro.sample(
-                f"s_obs_{i}_{j}", dist.Normal(s_pred, sigma_s), obs=s_obs
-            )
+    # observations and likelihood
+    for g in range(num_genes):
+        for c in range(num_cells):
+            with numpyro.plate(f"gene_{g}_cell_{c}_obs", num_timepoints):
+                numpyro.sample(
+                    f"u_obs_gene_{g}_cell_{c}",
+                    dist.Normal(u_pred[g, c], sigma[0]),
+                    obs=data[g, c, :, 0],
+                )
+                numpyro.sample(
+                    f"s_obs_gene_{g}_cell_{c}",
+                    dist.Normal(s_pred[g, c], sigma[1]),
+                    obs=data[g, c, :, 1],
+                )
 
 
 @beartype
 def generate_test_data_for_deterministic_model_inference(
-    num_cells: int,
     num_genes: int,
+    num_cells: int,
     num_timepoints: int,
     num_modalities: int,
+    log_time_start: float = -1,
+    log_time_end: float = 1,
+    noise_levels: Tuple[float, float] = (0.05, 0.05),
+    initial_conditions_params: Tuple[float, float] = (1.0, 0.1),
+    gamma_params: Tuple[float, float] = (2.0, 0.5),
 ) -> Tuple[
     TimeTensor,
     MultiModalTranscriptomeTensor,
@@ -131,6 +137,12 @@ def generate_test_data_for_deterministic_model_inference(
         num_genes (Integer): Number of genes
         num_timepoints (Integer): Number of timepoints
         num_modalities (Integer): Number of modalities
+        log_time_start (float): Log-space start for time points.
+        log_time_end (float): Log-space end for time points.
+        noise_levels (Tuple[float, float]): Standard deviations for observational noise on each modality.
+        initial_conditions_params (Tuple[float, float]): Parameters (mean, std) for log-normal initial conditions.
+        gamma_params (Tuple[float, float]): Parameters (mean, std) for log-normal gamma distribution.
+
 
     Returns:
         Tuple[
@@ -144,52 +156,101 @@ def generate_test_data_for_deterministic_model_inference(
 
     Example:
         >>> # xdoctest: +SKIP
-        >>> time_array, data, num_cells, num_genes, num_timepoints = (
+        >>> times, data, num_cells, num_genes, num_timepoints, num_modalities = (
         >>> generate_test_data_for_deterministic_model_inference(
-        ...      num_cells=3,
         ...      num_genes=1,
-        ...      num_timepoints=5,
+        ...      num_cells=3,
+        ...      num_timepoints=4,
         ...      num_modalities=2,
         >>> )
     """
-    # simulate timepoints for each cell as a proxy for sampling
-    times = jnp.logspace(-1, 1, num_timepoints)
-    time_array = jnp.tile(times, (num_cells, 1))
-
-    # set common initial conditions and parameters
-    initial_conditions = jnp.array([0.1, 0.1])
-    params = jnp.array([1.00])
-
-    # initialize data tensor
-    data = jnp.zeros((num_cells, num_genes, len(times), num_modalities))
-
     rng_key = jax.random.PRNGKey(0)
     rng_key, rng_key_ = jax.random.split(rng_key)
 
-    for i in range(num_genes):
-        # solve the ODEs for each gene
+    # simulate timepoints for each cell to represent observational sampling
+    total_timepoints = num_timepoints * num_cells
+    all_times = jnp.logspace(
+        log_time_start,
+        log_time_end,
+        total_timepoints,
+    )
+    time_indices = jax.random.choice(
+        rng_key_,
+        total_timepoints,
+        shape=(num_cells, num_timepoints),
+        replace=False,
+    )
+    time_indices = jnp.sort(time_indices, axis=1)
+    row_order = jnp.argsort(time_indices[:, 0])
+    # Each row is sorted
+    # The rows are sorted by the first column
+    # Array([[ 0,  2,  4, 10],
+    #        [ 1,  5,  8, 11],
+    #        [ 3,  6,  7,  9]], dtype=int32)
+    time_indices = time_indices[row_order]
+    times = all_times[time_indices]
+
+    # Flatten times and sort them globally while retaining
+    # the original order associated to the observation times
+    # of each cell
+    flat_times = times.flatten()
+    sorted_indices = jnp.argsort(flat_times)
+    sorted_times = flat_times[sorted_indices]
+
+    initial_conditions = numpyro.sample(
+        "initial_conditions",
+        dist.LogNormal(
+            loc=jnp.log(initial_conditions_params[0]),
+            scale=initial_conditions_params[1],
+        ),
+        sample_shape=(num_genes, num_modalities),
+        rng_key=rng_key_,
+    )
+    gamma = numpyro.sample(
+        "gamma",
+        dist.LogNormal(
+            loc=jnp.log(gamma_params[0]),
+            scale=gamma_params[1],
+        ),
+        sample_shape=(num_genes,),
+        rng_key=rng_key_,
+    )
+
+    # Preparing for vectorized ODE solving
+    def model_solver(ts, init_cond, rate):
         solution = solve_transcription_splicing_model(
-            times, initial_conditions, params
+            ts=ts.reshape(-1),  # Flatten the time array for all cells
+            initial_state=init_cond,
+            params=(rate,),
+        ).ys
+
+        return solution[sorted_indices.argsort()].reshape(
+            num_cells, num_timepoints, num_modalities
         )
-        u_simulated, s_simulated = solution.ys[:, 0], solution.ys[:, 1]
 
-        # add noise to the simulated data
-        noise_level_u = 0.05
-        noise_level_s = 0.05
-        u_noisy = dist.Normal(u_simulated, noise_level_u).sample(rng_key_)
-        s_noisy = dist.Normal(s_simulated, noise_level_s).sample(rng_key_)
+    # Compute the ODE solutions for all genes
+    solutions = jax.vmap(
+        model_solver,
+        in_axes=(None, 0, 0),
+    )(
+        sorted_times,
+        initial_conditions,
+        gamma,
+    )
 
-        for j in range(num_cells):
-            data = data.at[j, i, :, 0].set(u_noisy)
-            data = data.at[j, i, :, 1].set(s_noisy)
+    # Adding observational noise
+    noisy_solutions = solutions + jax.random.normal(
+        rng_key_, solutions.shape
+    ) * jnp.array(noise_levels)
 
-    logger.info(f"\nTest data tensor shape: {data.shape}\n")
-    logger.info(f"\nTest data time tensor shape: {time_array.shape}\n")
+    logger.info(f"Generated test data tensor shape: {noisy_solutions.shape}")
+    logger.info(f"Generated test time array shape: {times.shape}")
+
     return (
-        time_array,
-        data,
-        num_cells,
+        times,
+        noisy_solutions,
         num_genes,
+        num_cells,
         num_timepoints,
         num_modalities,
     )
