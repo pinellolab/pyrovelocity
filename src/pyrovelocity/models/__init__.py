fa27940fab4d46f52818e0034c3cb6837d05a105
Cameron Smith <cameron.ray.smith@gmail.com>
2024-04-13 03:59:29 -0400

    fix(models): add deterministic inference model to public interface Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/models/__init__.py
+++ src/pyrovelocity/models/__init__.py
@@ -1,3 +1,6 @@
+from pyrovelocity.models._deterministic_inference import (
+    deterministic_transcription_splicing_probabilistic_model,
+)
 from pyrovelocity.models._deterministic_simulation import (
     solve_transcription_splicing_model,
 )
@@ -9,6 +12,7 @@ from pyrovelocity.models._velocity import PyroVelocity
 
 
 __all__ = [
+    deterministic_transcription_splicing_probabilistic_model,
     mrna_dynamics,
     PyroVelocity,
     solve_transcription_splicing_model,
