9d2f67d7b82f1de2b2c1268a0bdb812d410bcfbf
Alexander Aivazidis <alexander.aivazidis@sanger.ac.uk>
2024-07-08 21:22:18 +0000

    added atac_layer argument to train_model tasks and made tests for it

---
+++ src/pyrovelocity/tests/tasks/test_train_model.py
@@ -0,0 +1,20 @@
+"""Tests for `pyrovelocity._train_model` task."""
+
+from pyrovelocity.tasks.train import train_model
+from pyrovelocity.utils import generate_sample_data
+from pyrovelocity.tasks.preprocess import copy_raw_counts
+
+
+def test_train_model(tmp_path):
+    loss_plot_path = str(tmp_path) + "/loss_plot_docs.png"
+    print(loss_plot_path)
+    adata = generate_sample_data(random_seed=99)
+    copy_raw_counts(adata)
+    _, model, posterior_samples = train_model(
+        adata,
+        atac_layer="atac",
+        use_gpu="auto",
+        seed=99,
+        max_epochs=200,
+        loss_plot_path=loss_plot_path,
+    )
