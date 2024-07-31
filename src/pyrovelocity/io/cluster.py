b98ca30b974e0966e2da6ccae6a73740e0689ccf
Cameron Smith <cameron.ray.smith@gmail.com>
2024-07-31 12:00:34 -0400

    feat(io): add cluster module
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

---
+++ src/pyrovelocity/io/cluster.py
@@ -0,0 +1,54 @@
+from typing import Tuple
+
+from beartype import beartype
+from flytekit.configuration import Config
+from flytekit.interaction.string_literals import literal_map_string_repr
+from flytekit.remote.remote import FlyteRemote
+from omegaconf import DictConfig, OmegaConf
+
+from pyrovelocity.utils import print_config_tree
+
+__all__ = ["get_remote_task_results"]
+
+
+@beartype
+def get_remote_task_results(
+    execution_id: str,
+    task_id: str,
+    endpoint: str = "flyte.cluster.pyrovelocity.net",
+    remote_uri_protocol: str = "flyte://v1",
+    remote_project: str = "pyrovelocity",
+    remote_domain: str = "development",
+) -> Tuple[DictConfig, DictConfig]:
+    """
+    Get the inputs and outputs of a remote Flyte task.
+
+    Args:
+        execution_id (str): Flyte execution id.
+        task_id (str): Flyte task id. Defaults to "f26c1pjy-0-dn1-0-dn6".
+        endpoint (str): Cluster endpoint url. Defaults to "flyte.cluster.pyrovelocity.net".
+        remote_uri_protocol (str): Flyte remote uri protocol. Defaults to "flyte://v1".
+        remote_project (str): Flyte remote project. Defaults to "pyrovelocity".
+        remote_domain (str): Flyte remote domain. Defaults to "development".
+
+    Returns:
+        Tuple[DictConfig, DictConfig]: Autocompletable inputs and outputs of
+            the remote task execution.
+    """
+    remote = FlyteRemote(
+        Config.for_endpoint(endpoint=endpoint),
+    )
+    execution_uri = (
+        f"{remote_uri_protocol}/{remote_project}/{remote_domain}/{execution_id}"
+    )
+    task_uri = f"{execution_uri}/{task_id}"
+    inputs = remote.get(f"{task_uri}/i")
+    outputs = remote.get(f"{task_uri}/o")
+    inputs_dict = literal_map_string_repr(inputs.literals)
+    inputs_dictconfig = OmegaConf.create(inputs_dict)
+    print_config_tree(inputs_dict)
+
+    outputs_dict = literal_map_string_repr(outputs.literals)
+    outputs_dictconfig = OmegaConf.create(outputs_dict)
+    print_config_tree(outputs_dict)
+    return inputs_dictconfig, outputs_dictconfig
