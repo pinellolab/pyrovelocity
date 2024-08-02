9bdc4ee860a95c6b6fd321df745abb2e93075970
Cameron Smith <cameron.ray.smith@gmail.com>
2024-08-02 14:45:56 -0400

    test(tasks): add preprocess and training fixtures to subpackage tests
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/tests/tasks/conftest.py
+++ src/pyrovelocity/tests/tasks/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 
 from pyrovelocity.tasks.data import download_dataset
+from pyrovelocity.tasks.preprocess import preprocess_dataset
+from pyrovelocity.tasks.train import train_dataset
 
 
 @pytest.fixture
@@ -20,4 +22,23 @@ def simulated_dataset_path(tmp_tasks_dir):
         source="simulate",
         n_obs=100,
         n_vars=200,
+    )
+
+
+@pytest.fixture
+def preprocess_dataset_output(simulated_dataset_path, tmp_tasks_dir):
+    return preprocess_dataset(
+        data_set_name="simulated",
+        adata=simulated_dataset_path,
+        data_processed_path=tmp_tasks_dir / "data/processed",
+    )
+
+
+@pytest.fixture
+def train_dataset_output(preprocess_dataset_output, tmp_tasks_dir):
+    _, preprocessed_dataset_path = preprocess_dataset_output
+    return train_dataset(
+        adata=preprocessed_dataset_path,
+        models_path=tmp_tasks_dir / "models",
+        max_epochs=200,
     )
