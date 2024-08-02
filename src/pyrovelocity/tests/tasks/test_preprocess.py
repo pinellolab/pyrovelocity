057333f5c5ef27c29b1786845ac16457cf519f05
Cameron Smith <cameron.ray.smith@gmail.com>
2024-08-01 23:04:06 -0400

    test(tasks): add slow call to preprocess function
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/tests/tasks/test_preprocess.py
+++ src/pyrovelocity/tests/tasks/test_preprocess.py
@@ -1,7 +1,19 @@
 """Tests for `pyrovelocity.tasks.preprocess` module."""
+import pytest
+
+from pyrovelocity.tasks.preprocess import preprocess_dataset
 
 
 def test_load_preprocess():
     from pyrovelocity.tasks import preprocess
 
     print(preprocess.__file__)
+
+
+@pytest.mark.slow
+def test_preprocess_dataset(simulated_dataset_path, tmp_tasks_dir):
+    preprocess_dataset(
+        data_set_name="simulated",
+        adata=simulated_dataset_path,
+        data_processed_path=tmp_tasks_dir / "data/processed",
+    )
