6cc63b8d6c3c9623e15549cc2c119955b2571e95
Cameron Smith <cameron.ray.smith@gmail.com>
2024-08-03 17:13:30 -0400

    test(plots): run plot gene ranking
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

---
+++ src/pyrovelocity/tests/plots/test_genes.py
@@ -0,0 +1,39 @@
+import pytest
+
+from pyrovelocity.plots import plot_gene_ranking
+
+
+@pytest.mark.slow
+def test_model2_plot_gene_ranking(
+    postprocessed_model2_data,
+    posterior_samples_model2,
+    putative_model2_marker_genes,
+    tmp_data_dir,
+):
+    plot_gene_ranking(
+        posterior_samples=[posterior_samples_model2],
+        adata=[postprocessed_model2_data],
+        selected_genes=putative_model2_marker_genes,
+        time_correlation_with="st",
+        show_marginal_histograms=True,
+        save_volcano_plot=True,
+        volcano_plot_path=tmp_data_dir / "volcano.pdf",
+    )
+
+
+@pytest.mark.slow
+def test_model1_plot_gene_ranking(
+    postprocessed_model1_data,
+    posterior_samples_model1,
+    putative_model1_marker_genes,
+    tmp_data_dir,
+):
+    plot_gene_ranking(
+        posterior_samples=[posterior_samples_model1],
+        adata=[postprocessed_model1_data],
+        selected_genes=putative_model1_marker_genes,
+        time_correlation_with="st",
+        show_marginal_histograms=True,
+        save_volcano_plot=True,
+        volcano_plot_path=tmp_data_dir / "volcano.pdf",
+    )
