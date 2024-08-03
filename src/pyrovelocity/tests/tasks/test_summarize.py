e41a46a4cf258b6c4845b78f8f2f617cef69c67b
Cameron Smith <cameron.ray.smith@gmail.com>
2024-08-03 10:53:25 -0400

    test(tasks): return summarize dataset output
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/tests/tasks/test_summarize.py
+++ src/pyrovelocity/tests/tasks/test_summarize.py
@@ -1,7 +1,16 @@
 """Tests for `pyrovelocity.tasks.summarize` module."""
 
 
+import pytest
+
+
 def test_load_summarize():
     from pyrovelocity.tasks import summarize
 
     print(summarize.__file__)
+
+
+@pytest.mark.slow
+def test_summarize_dataset(summarize_dataset_output):
+    return summarize_dataset_output
+
