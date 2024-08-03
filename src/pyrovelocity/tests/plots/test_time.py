93728537cdcdb790f87137b3cf0920ce19ff9f2f
Cameron Smith <cameron.ray.smith@gmail.com>
2024-08-03 16:46:30 -0400

    test(plots): use in-memory fixtures for shared time plots
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/tests/plots/test_time.py
+++ src/pyrovelocity/tests/plots/test_time.py
@@ -1,21 +1,18 @@
 import pytest
-import scanpy as sc
 
-from pyrovelocity.io.compressedpickle import CompressedPickle
 from pyrovelocity.plots import plot_shared_time_uncertainty
 
 
 @pytest.mark.slow
 def test_model2_plot_shared_time_uncertainty(
-    postprocess_dataset_output,
+    postprocessed_model2_data,
+    posterior_samples_model2,
     tmp_data_dir,
 ):
-    posterior_samples = CompressedPickle.load(postprocess_dataset_output[0])
-    adata = sc.read(postprocess_dataset_output[1])
     shared_time_plot = tmp_data_dir / "shared_time_plot.pdf"
     plot_shared_time_uncertainty(
-        adata=adata,
-        posterior_samples=posterior_samples,
+        adata=postprocessed_model2_data,
+        posterior_samples=posterior_samples_model2,
         vector_field_basis="umap",
         shared_time_plot=shared_time_plot,
     )
@@ -23,16 +20,14 @@ def test_model2_plot_shared_time_uncertainty(
 
 @pytest.mark.slow
 def test_model1_plot_shared_time_uncertainty(
-    postprocess_dataset_model1_output,
+    postprocessed_model1_data,
+    posterior_samples_model1,
     tmp_data_dir,
 ):
-    postprocess_dataset_output = postprocess_dataset_model1_output
-    posterior_samples = CompressedPickle.load(postprocess_dataset_output[0])
-    adata = sc.read(postprocess_dataset_output[1])
     shared_time_plot = tmp_data_dir / "shared_time_plot.pdf"
     plot_shared_time_uncertainty(
-        adata=adata,
-        posterior_samples=posterior_samples,
+        adata=postprocessed_model1_data,
+        posterior_samples=posterior_samples_model1,
         vector_field_basis="umap",
         shared_time_plot=shared_time_plot,
     )
