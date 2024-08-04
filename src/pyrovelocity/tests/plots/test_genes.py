30d7d9fb4940753d403f9d3316684d5f991568e3
Cameron Smith <cameron.ray.smith@gmail.com>
2024-08-04 13:51:09 -0400

    test(plots): use session-level reports path for gene selection plots
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/tests/plots/test_genes.py
+++ src/pyrovelocity/tests/plots/test_genes.py
@@ -8,7 +8,7 @@ def test_model2_plot_gene_ranking(
     postprocessed_model2_data,
     posterior_samples_model2,
     putative_model2_marker_genes,
-    tmp_data_dir,
+    data_model2_reports_path,
 ):
     plot_gene_ranking(
         posterior_samples=[posterior_samples_model2],
@@ -17,7 +17,7 @@ def test_model2_plot_gene_ranking(
         time_correlation_with="st",
         show_marginal_histograms=True,
         save_volcano_plot=True,
-        volcano_plot_path=tmp_data_dir / "volcano.pdf",
+        volcano_plot_path=data_model2_reports_path / "volcano.pdf",
     )
 
 
@@ -26,7 +26,7 @@ def test_model1_plot_gene_ranking(
     postprocessed_model1_data,
     posterior_samples_model1,
     putative_model1_marker_genes,
-    tmp_data_dir,
+    data_model1_reports_path,
 ):
     plot_gene_ranking(
         posterior_samples=[posterior_samples_model1],
@@ -35,5 +35,5 @@ def test_model1_plot_gene_ranking(
         time_correlation_with="st",
         show_marginal_histograms=True,
         save_volcano_plot=True,
-        volcano_plot_path=tmp_data_dir / "volcano.pdf",
+        volcano_plot_path=data_model1_reports_path / "volcano.pdf",
     )
