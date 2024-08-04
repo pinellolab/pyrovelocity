dd57ebd417b585607e105137258a40f2fc453086
Cameron Smith <cameron.ray.smith@gmail.com>
2024-08-04 13:50:43 -0400

    test(plots): use session-level reports path for shared time plot
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/tests/plots/test_time.py
+++ src/pyrovelocity/tests/plots/test_time.py
@@ -7,9 +7,9 @@ from pyrovelocity.plots import plot_shared_time_uncertainty
 def test_model2_plot_shared_time_uncertainty(
     postprocessed_model2_data,
     posterior_samples_model2,
-    tmp_data_dir,
+    data_model2_reports_path,
 ):
-    shared_time_plot = tmp_data_dir / "shared_time_plot.pdf"
+    shared_time_plot = data_model2_reports_path / "shared_time_plot.pdf"
     plot_shared_time_uncertainty(
         adata=postprocessed_model2_data,
         posterior_samples=posterior_samples_model2,
@@ -22,9 +22,9 @@ def test_model2_plot_shared_time_uncertainty(
 def test_model1_plot_shared_time_uncertainty(
     postprocessed_model1_data,
     posterior_samples_model1,
-    tmp_data_dir,
+    data_model1_reports_path,
 ):
-    shared_time_plot = tmp_data_dir / "shared_time_plot.pdf"
+    shared_time_plot = data_model1_reports_path / "shared_time_plot.pdf"
     plot_shared_time_uncertainty(
         adata=postprocessed_model1_data,
         posterior_samples=posterior_samples_model1,
