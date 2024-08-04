d1559e9d3f73a0817529933bf9d3bf7f7a2009f8
Cameron Smith <cameron.ray.smith@gmail.com>
2024-08-04 13:50:57 -0400

    test(plots): use session-level reports path for predictive plots
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/tests/plots/test_predictive.py
+++ src/pyrovelocity/tests/plots/test_predictive.py
@@ -9,7 +9,7 @@ def test_model2_plot_posterior_predictive(
     posterior_samples_model2,
     putative_model2_marker_genes,
     train_dataset_output,
-    tmp_data_dir,
+    data_model2_reports_path,
 ):
     posterior_curve(
         adata=postprocessed_model2_data,
@@ -17,7 +17,7 @@ def test_model2_plot_posterior_predictive(
         gene_set=putative_model2_marker_genes,
         data_model=train_dataset_output[0],
         model_path=train_dataset_output[3],
-        output_directory=tmp_data_dir / "results" / "posterior_phase_portraits",
+        output_directory=data_model2_reports_path / "posterior_phase_portraits",
     )
 
 
@@ -27,13 +27,12 @@ def test_model1_plot_posterior_predictive(
     posterior_samples_model1,
     putative_model1_marker_genes,
     train_dataset_model1_output,
-    tmp_data_dir,
-):
+    data_model1_reports_path):
     posterior_curve(
         adata=postprocessed_model1_data,
         posterior_samples=posterior_samples_model1,
         gene_set=putative_model1_marker_genes,
         data_model=train_dataset_model1_output[0],
         model_path=train_dataset_model1_output[3],
-        output_directory=tmp_data_dir / "results" / "posterior_phase_portraits",
+        output_directory=data_model1_reports_path / "posterior_phase_portraits",
     )
