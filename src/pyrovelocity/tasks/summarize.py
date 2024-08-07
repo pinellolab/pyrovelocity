583f7869e62145e718a9447da8d4b2820d349a68
Cameron Smith <cameron.ray.smith@gmail.com>
2024-08-07 19:45:23 -0400

    fix(tasks): remove unused t0 from model1 posterior samples
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/tasks/summarize.py
+++ src/pyrovelocity/tasks/summarize.py
@@ -214,6 +214,9 @@ def summarize_dataset(
     volcano_data: DataFrame = posterior_samples["gene_ranking"]
     volcano_data.to_csv(gene_ranking_path)
 
+    if "model1" in data_model:
+        posterior_samples.pop("t0", None)
+
     ##################
     # generate figures
     ##################
