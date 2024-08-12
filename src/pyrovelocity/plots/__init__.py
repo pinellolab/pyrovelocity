a36ad39e1862b792fa8a2d8506773886f22bea7c
Cameron Smith <cameron.ray.smith@gmail.com>
2024-08-12 13:29:02 -0400

    chore(plots): import count histogram to plots subpackage
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/plots/__init__.py
+++ src/pyrovelocity/plots/__init__.py
@@ -1,7 +1,8 @@
-from pyrovelocity.plots._dynamics import (
-    plot_deterministic_simulation_phase_portrait,
+from pyrovelocity.plots._count_histograms import (
+    plot_spliced_unspliced_histogram,
 )
 from pyrovelocity.plots._dynamics import (
+    plot_deterministic_simulation_phase_portrait,
     plot_deterministic_simulation_trajectories,
 )
 from pyrovelocity.plots._experimental import plot_t0_selection
@@ -11,19 +12,20 @@ from pyrovelocity.plots._parameters import (
 )
 from pyrovelocity.plots._predictive import (
     extrapolate_prediction_sample_predictive,
+    posterior_curve,
 )
-from pyrovelocity.plots._predictive import posterior_curve
 from pyrovelocity.plots._rainbow import rainbowplot
 from pyrovelocity.plots._summary import plot_gene_selection_summary
-from pyrovelocity.plots._time import plot_posterior_time
-from pyrovelocity.plots._time import plot_shared_time_uncertainty
-from pyrovelocity.plots._uncertainty import cluster_violin_plots
-from pyrovelocity.plots._uncertainty import (
-    get_posterior_sample_angle_uncertainty,
+from pyrovelocity.plots._time import (
+    plot_posterior_time,
+    plot_shared_time_uncertainty,
 )
-from pyrovelocity.plots._uncertainty import plot_state_uncertainty
+from pyrovelocity.plots._uncertainty import (
+    cluster_violin_plots,
+    get_posterior_sample_angle_uncertainty,
+    plot_state_uncertainty,
+)
 from pyrovelocity.plots._vector_fields import plot_vector_field_summary
-
 
 __all__ = [
     "cluster_violin_plots",
@@ -35,6 +37,7 @@ __all__ = [
     "plot_parameter_posterior_distributions",
     "plot_posterior_time",
     "plot_shared_time_uncertainty",
+    "plot_spliced_unspliced_histogram",
     "plot_state_uncertainty",
     "plot_t0_selection",
     "posterior_curve",
