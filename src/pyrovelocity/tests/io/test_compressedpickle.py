99f165735993c728db4701721f26ceeffa871f3b
Cameron Smith <cameron.ray.smith@gmail.com>
2024-08-14 14:08:01 -0400

    test(io): add test cases for automatic sparsification of array dictionaries
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/tests/io/test_compressedpickle.py
+++ src/pyrovelocity/tests/io/test_compressedpickle.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 import pytest
+from sparse import COO
 
 from pyrovelocity.io.compressedpickle import CompressedPickle
 
@@ -84,3 +85,99 @@ def cleanup(request, test_file):
             os.remove(test_file)
 
     request.addfinalizer(delete_test_file)
+
+
+@pytest.fixture
+def sparse_dict():
+    return {
+        "dense": np.array([[1, 2], [3, 4]]),
+        "sparse": np.array([[1, 0], [0, 2]]),
+        "very_sparse": np.array([[0, 1], [0, 0]]),
+    }
+
+
+def test_save_load_sparse_dict(test_file, sparse_dict):
+    CompressedPickle.save(test_file, sparse_dict)
+    loaded_dict = CompressedPickle.load(test_file)
+
+    assert isinstance(loaded_dict["dense"], np.ndarray)
+    assert isinstance(loaded_dict["sparse"], np.ndarray)
+    assert isinstance(loaded_dict["very_sparse"], np.ndarray)
+
+    assert np.array_equal(loaded_dict["dense"], sparse_dict["dense"])
+    assert np.array_equal(loaded_dict["sparse"], sparse_dict["sparse"])
+    assert np.array_equal(
+        loaded_dict["very_sparse"], sparse_dict["very_sparse"]
+    )
+
+
+def test_save_load_sparse_dict_without_densify(test_file, sparse_dict):
+    CompressedPickle.save(test_file, sparse_dict)
+    loaded_dict = CompressedPickle.load(test_file, densify=False)
+
+    assert isinstance(loaded_dict["dense"], np.ndarray)
+    assert isinstance(loaded_dict["sparse"], COO)
+    assert isinstance(loaded_dict["very_sparse"], COO)
+
+    assert np.array_equal(loaded_dict["dense"], sparse_dict["dense"])
+    assert np.array_equal(
+        loaded_dict["sparse"].todense(), sparse_dict["sparse"]
+    )
+    assert np.array_equal(
+        loaded_dict["very_sparse"].todense(), sparse_dict["very_sparse"]
+    )
+
+
+def test_save_without_sparsify(test_file, sparse_dict):
+    CompressedPickle.save(test_file, sparse_dict, sparsify=False)
+    loaded_dict = CompressedPickle.load(test_file)
+
+    assert all(isinstance(arr, np.ndarray) for arr in loaded_dict.values())
+    assert all(
+        np.array_equal(loaded_dict[key], sparse_dict[key])
+        for key in sparse_dict
+    )
+
+
+# TODO: Handle sparsification when values are not exclusively arrays
+def test_save_load_mixed_dict(test_file):
+    mixed_dict = {
+        "dense": np.array([[1, 2], [3, 4]]),
+        "sparse": COO.from_numpy(np.array([[1, 0], [0, 2]])),
+        "very_sparse": COO.from_numpy(np.array([[0, 1], [0, 0]])),
+        "non_array": "test string",
+    }
+
+    CompressedPickle.save(test_file, mixed_dict)
+    loaded_dict = CompressedPickle.load(test_file)
+
+    assert isinstance(loaded_dict["dense"], np.ndarray)
+    # assert isinstance(loaded_dict["sparse"], np.ndarray)
+    # assert isinstance(loaded_dict["very_sparse"], np.ndarray)
+    assert isinstance(loaded_dict["sparse"], COO)
+    assert isinstance(loaded_dict["very_sparse"], COO)
+    assert isinstance(loaded_dict["non_array"], str)
+
+    assert np.array_equal(loaded_dict["dense"], mixed_dict["dense"])
+    # assert np.array_equal(loaded_dict["sparse"], mixed_dict["sparse"].todense())
+    # assert np.array_equal(
+    #     loaded_dict["very_sparse"], mixed_dict["very_sparse"].todense()
+    # )
+    assert np.array_equal(
+        loaded_dict["sparse"].todense(),
+        mixed_dict["sparse"].todense(),
+    )
+    assert np.array_equal(
+        loaded_dict["very_sparse"].todense(),
+        mixed_dict["very_sparse"].todense(),
+    )
+    assert loaded_dict["non_array"] == mixed_dict["non_array"]
+
+
+def test_save_load_non_dict(test_file):
+    non_dict_obj = np.array([[1, 2], [3, 4]])
+    CompressedPickle.save(test_file, non_dict_obj)
+    loaded_obj = CompressedPickle.load(test_file)
+
+    assert isinstance(loaded_obj, np.ndarray)
+    assert np.array_equal(loaded_obj, non_dict_obj)
