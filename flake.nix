7798b2542562dd53bd751cb660e22c41b1923100
Cameron Smith <cameron.ray.smith@gmail.com>
2025-03-01 16:00:58 -0500

    fix(flake): temp pin nixpkgs to 24.11-beta
    - pins poetry and poetry-core <2.0
    
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.11-beta";
     systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-utils.url = github:numtide/flake-utils;
