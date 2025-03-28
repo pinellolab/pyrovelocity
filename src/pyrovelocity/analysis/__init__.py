16d382ea3b6b1f13e69e190d04adfcb41d54d561
Cameron Smith <cameron.ray.smith@gmail.com>
2025-03-28 16:40:13 -0400

    fix(analysis): include trajectory functions
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/analysis/__init__.py
+++ src/pyrovelocity/analysis/__init__.py
@@ -1,10 +1,15 @@
 import pyrovelocity.analysis.analyze
 import pyrovelocity.analysis.cytotrace
 import pyrovelocity.analysis.transcriptome_properties
-
+from pyrovelocity.analysis.trajectory import (
+    align_trajectory_diff,
+    get_clone_trajectory,
+)
 
 __all__ = [
     "analyze",
     "cytotrace",
     "transcriptome_properties",
+    "align_trajectory_diff",
+    "get_clone_trajectory",
 ]
