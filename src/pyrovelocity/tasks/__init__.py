21d3e187f880aaaa55809ae4b396fb861a74c0b7
Cameron Smith <cameron.ray.smith@gmail.com>
2025-03-31 22:35:57 -0400

    fix(tasks): export evaluation task
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/pyrovelocity/tasks/__init__.py
+++ src/pyrovelocity/tasks/__init__.py
@@ -1,12 +1,13 @@
 import pyrovelocity.tasks.data
+import pyrovelocity.tasks.evaluate
 import pyrovelocity.tasks.postprocess
 import pyrovelocity.tasks.preprocess
 import pyrovelocity.tasks.summarize
 import pyrovelocity.tasks.train
 
-
 __all__ = [
     "data",
+    "evaluate",
     "postprocess",
     "preprocess",
     "summarize",
