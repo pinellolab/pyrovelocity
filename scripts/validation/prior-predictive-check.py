cbd8077aca8f243975edd1f383bc114fd17963f2
Cameron Smith <cameron.ray.smith@gmail.com>
2025-06-09 17:21:03 -0400

    scripts: set default font size 7 -> 5 for predictive checks
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- scripts/validation/prior-predictive-check.py
+++ scripts/validation/prior-predictive-check.py
@@ -66,7 +66,7 @@ fig_prior = plot_prior_predictive_checks(
     save_path="reports/docs/prior_predictive",
     figure_name=f"piecewise_activation_prior_checks_{RANDOM_SEED}",
     combine_individual_pdfs=True,
-    default_fontsize=7,
+    default_fontsize=5,
 )
 
 plt.suptitle("Prior Predictive Checks: Piecewise Activation Model", fontsize=16)
